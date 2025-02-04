be1b945c11bb5c82f05552fd471fa3ea1216458b
Seio924 <seio9241@gmail.com>
2025-02-05 02:32:27 +0900

    style(#3): 폰트 사이즈 수정 및 Pretendard 적용 오류 해결

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,129 +1,97 @@
 import { css } from '@emotion/react';
 
 const typography = {
-  family: {
-    primary: 'Pretendard',
-    code: 'SF Mono, monospace',
-  },
-  accent: 600,
-
   heading: {
     900: css`
+      font-family: 'Pretendard';
       font-weight: 700;
-      font-size: 3.5rem;
-      line-height: 4rem;
+      font-size: 4.9rem;
       letter-spacing: 0;
     `,
     800: css`
+      font-family: 'Pretendard';
       font-weight: 700;
-      font-size: 2.9rem;
-      line-height: 3.2rem;
+      font-size: 4.06rem;
       letter-spacing: 0;
     `,
     700: css`
+      font-family: 'Pretendard';
       font-weight: 700;
-      font-size: 2.4rem;
-      line-height: 2.8rem;
+      font-size: 3.36rem;
       letter-spacing: 0;
     `,
     600: css`
+      font-family: 'Pretendard';
       font-weight: 700;
-      font-size: 2rem;
-      line-height: 2.4rem;
+      font-size: 2.8rem;
       letter-spacing: 0;
     `,
     500: css`
+      font-family: 'Pretendard';
       font-weight: 600;
-      font-size: 1.6rem;
-      line-height: 2rem;
+      font-size: 2.24rem;
       letter-spacing: 0;
     `,
     400: css`
+      font-family: 'Pretendard';
       font-weight: 600;
-      font-size: 1.4rem;
-      line-height: 1.6rem;
+      font-size: 1.96rem;
       letter-spacing: 0;
     `,
     300: css`
+      font-family: 'Pretendard';
       font-weight: 600;
-      font-size: 1.2rem;
-      line-height: 1.6rem;
+      font-size: 1.68rem;
       letter-spacing: 0;
     `,
     200: css`
+      font-family: 'Pretendard';
       font-weight: 600;
-      font-size: 1.2rem;
-      line-height: 1.6rem;
+      font-size: 1.68rem;
       letter-spacing: 0;
     `,
     100: css`
+      font-family: 'Pretendard';
       font-weight: 600;
-      font-size: 1.1rem;
-      line-height: 1.6rem;
+      font-size: 1.54rem;
       letter-spacing: 0;
     `,
   },
 
   common: {
     default: css`
+      font-family: 'Pretendard';
       font-weight: 400;
       font-size: 1.4rem;
       line-height: 2rem;
       letter-spacing: 0;
     `,
-    paragraph: css`
-      font-weight: 400;
-      font-size: 1.4rem;
-      line-height: 2.4rem;
-      letter-spacing: 0;
-    `,
     block: css`
+      font-family: 'Pretendard';
       font-weight: 500;
       font-size: 1.4rem;
       line-height: 2rem;
       letter-spacing: 0;
-      margin-bottom: 2.8rem;
     `,
-    code: css`
-      font-family: 'SF Mono', monospace;
-      font-weight: 400;
-      font-size: 1.2rem;
+    defaultAccent: css`
+      font-family: 'Pretendard';
+      font-weight: 600;
+      font-size: 1.4rem;
       line-height: 2rem;
+      letter-spacing: 0;
     `,
     small: css`
+      font-family: 'Pretendard';
       font-weight: 400;
       font-size: 1.2rem;
       line-height: 1.4rem;
       letter-spacing: 0;
     `,
     smallAccent: css`
-      font-weight: 700;
+      font-family: 'Pretendard';
+      font-weight: 600;
       font-size: 1.2rem;
       line-height: 1.4rem;
-      letter-spacing: 0;
-    `,
-    smallBlock: css`
-      font-weight: 500;
-      font-size: 1.2rem;
-      line-height: 2rem;
-      letter-spacing: 0;
-    `,
-    large: css`
-      font-weight: 400;
-      font-size: 1.6rem;
-      line-height: 2rem;
-      letter-spacing: 0;
-    `,
-    largeAccent: css`
-      font-weight: 700;
-      font-size: 1.6rem;
-      line-height: 2rem;
-      letter-spacing: 0;
-    `,
-    largeBlock: css`
-      font-weight: 500;
-      font-size: 1.6rem;
-      line-height: 2rem;
       letter-spacing: 0;
     `,
   },
