62a2c00b7387ca7cd9d9536fbd06a25dcc981771
Seio924 <seio9241@gmail.com>
2025-02-04 20:10:45 +0900

    style(#3): globalStyle 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Global } from '@emotion/react';
 import App from './App.tsx';
+import globalStyles from './styles/globalStyles.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global styles={globalStyles()} />
     <App />
   </StrictMode>,
 );
