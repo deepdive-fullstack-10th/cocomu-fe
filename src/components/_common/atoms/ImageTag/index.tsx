9bfd0c635ad27a79e342e886ec565882e0b79a8e
Seio924 <seio9241@gmail.com>
2025-02-05 19:02:41 +0900

    coc-89-refactor(#12): 언어별 이미지 매핑 상수 constants로 분리

--- src/components/_common/atoms/ImageTag/index.tsx
+++ src/components/_common/atoms/ImageTag/index.tsx
@@ -1,15 +1,10 @@
+import { LANGUAGE_IMAGES } from '../../../../constants/constants';
 import S from './style';
 
 interface ImageTagProps {
   name?: string;
 }
 
-const IMAGE_MAP: Record<string, string> = {
-  Python: 'https://holaworld.io/images/languages/python.png',
-  JavaScript: 'https://holaworld.io/images/languages/javascript.png',
-  Java: 'https://holaworld.io/images/languages/java.png',
-};
-
 export default function ImageTag({ name = 'Python' }: ImageTagProps) {
-  return <S.ImageTag src={IMAGE_MAP[name]} />;
+  return <S.ImageTag src={LANGUAGE_IMAGES[name]} />;
 }
