28cdcf01b994adb65882a60a57fbae5496e1bbbd
Seio924 <seio9241@gmail.com>
2025-02-05 21:38:28 +0900

    coc-89-feat(#12): LANGUAGE_IMAGES에 C 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,5 @@ export const LANGUAGE_IMAGES: Record<string, string> = {
   Python: 'https://holaworld.io/images/languages/python.png',
   JavaScript: 'https://holaworld.io/images/languages/javascript.png',
   Java: 'https://holaworld.io/images/languages/java.png',
+  C: 'https://holaworld.io/images/languages/c.png',
 } as const;
