aadaa07723ae18e2e9db53376ed081d1666f87b2
Seio924 <seio9241@gmail.com>
2025-02-05 19:01:04 +0900

    coc-89-feat(#12): ImageTag 컴포넌트 구현

---
+++ src/components/_common/atoms/ImageTag/style.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const ImageTag = styled.img`
+  width: 2.6rem;
+  height: 2.6rem;
+  border-radius: 50%;
+  object-fit: contain;
+`;
+
+const S = {
+  ImageTag,
+};
+
+export default S;
