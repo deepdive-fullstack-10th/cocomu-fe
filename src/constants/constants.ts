5f3b2a4e8881fd965cbed03268fcfaba90f11485
Seio924 <seio9241@gmail.com>
2025-02-05 19:02:41 +0900

    coc-89-refactor(#12): 언어별 이미지 매핑 상수 constants로 분리

---
+++ src/constants/constants.ts
@@ -0,0 +1,5 @@
+export const LANGUAGE_IMAGES: Record<string, string> = {
+  Python: 'https://holaworld.io/images/languages/python.png',
+  JavaScript: 'https://holaworld.io/images/languages/javascript.png',
+  Java: 'https://holaworld.io/images/languages/java.png',
+} as const;
