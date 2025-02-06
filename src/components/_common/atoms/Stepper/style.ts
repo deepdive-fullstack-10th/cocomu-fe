145b76d75042b9c0d91afdb37a2c032c70915796
HYJIN\isacc <isacc7224@gmail.com>
2025-02-06 17:06:42 +0900

    COC-129 Stepper 컴포넌트 수정

--- src/components/_common/atoms/Stepper/style.ts
+++ src/components/_common/atoms/Stepper/style.ts
@@ -1,57 +1,20 @@
-import { css, SerializedStyles, Theme } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export type StepperColor = 'gray' | 'white' | 'primary';
-export type Size = 'sm' | 'md' | 'lg';
-
-export interface StepperStyleProps {
-  color: StepperColor;
-  size: Size;
-}
-
-const commonStyles = (theme: Theme) => css`
+const Stepper = styled.div<{ select?: boolean }>`
+  cursor: pointer;
   border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${theme.color.gray[950]};
-`;
-
-const colorStyles: { [key in StepperColor]: (theme: Theme) => SerializedStyles } = {
-  white: (theme: Theme) => css`
-    background-color: ${theme.color.gray[50]};
-  `,
-  gray: (theme: Theme) => css`
-    background-color: ${theme.color.gray[200]};
-  `,
-
-  primary: (theme: Theme) => css`
-    background-color: ${theme.color.primary[100]};
-  `,
-};
-
-const SizeStyles: { [key in Size]: (theme: Theme) => SerializedStyles } = {
-  lg: (theme: Theme) => css`
-    font: ${theme.font.heading[700]};
-    width: 6rem;
-    height: 6rem;
-  `,
-  md: (theme: Theme) => css`
-    font: ${theme.font.heading[400]};
-    width: 4rem;
-    height: 4rem;
-  `,
-  sm: (theme: Theme) => css`
-    font: ${theme.font.heading[200]};
-    width: 3rem;
-    height: 3rem;
-  `,
-};
-
-const Stepper = styled.div<StepperStyleProps>`
-  ${({ color, theme }) => colorStyles[color](theme)}
-  ${({ size, theme }) => SizeStyles[size](theme)}
-  ${({ theme }) => commonStyles(theme)}
+  font: ${({ theme }) => theme.font.common.small};
+  color: ${({ theme }) => theme.color.gray[950]};
+  width: 1rem;
+  height: 1rem;
+  padding: 1rem;
+  background: ${({ theme, select }) => (select ? theme.color.primary[100] : theme.color.gray[50])};
+  &:hover {
+    background: ${({ theme }) => theme.color.gray[200]};
+  }
 `;
 
 const S = {
