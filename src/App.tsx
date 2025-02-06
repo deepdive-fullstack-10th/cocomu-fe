2505b6a851068df42a65e80229e035c33a01ab46
HYJIN\isacc <isacc7224@gmail.com>
2025-02-06 16:08:51 +0900

    feat(#11): App.tsx 되돌리기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import ToastModal from './components/_common/atoms/Toast';
-
 export default function App() {
-  return (
-    <div>
-      <ToastModal
-        type='default'
-        message='코코님이 테스트코드를 수정하였습니다'
-      />
-    </div>
-  );
+  return <div>coocmu</div>;
 }
