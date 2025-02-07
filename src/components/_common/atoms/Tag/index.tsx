8457f3dad37a88d63c6c53533e37039ab9f674e7
HYJIN\isacc <isacc7224@gmail.com>
2025-02-07 12:04:40 +0900

    COC-130-refactor(#20): IconButton 컴포넌트 리펙토링

--- src/components/_common/atoms/Tag/index.tsx
+++ src/components/_common/atoms/Tag/index.tsx
@@ -3,6 +3,6 @@ import S, { TagStyleProps } from './style';
 
 export type TagProps = PropsWithChildren<React.ComponentProps<'div'> & TagStyleProps>;
 
-export default function Tag({ children, color }: TagProps) {
+export default function Tag({ children, color = 'gray' }: TagProps) {
   return <S.Tag color={color}>{children}</S.Tag>;
 }
