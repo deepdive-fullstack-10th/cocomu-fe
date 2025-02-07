a9a47e85f551076db9c7557a04670a3a576feb19
KimBeomJo <dongjun1315@naver.com>
2025-02-07 12:22:42 +0900

    COC-131-refactor(#22): sm 사이즈 추가

--- src/components/_common/atoms/SearchInput/index.tsx
+++ src/components/_common/atoms/SearchInput/index.tsx
@@ -1,18 +1,18 @@
 import { BsSearch } from 'react-icons/bs';
 import S from './style';
 
-type SizeType = 'md' | 'lg';
+type SizeType = 'sm' | 'md' | 'lg';
 
 interface SearchInputProps {
   placeholder?: string;
   size: SizeType;
 }
 
-export default function SearchInput({ placeholder = '', size }: SearchInputProps) {
+export default function SearchInput({ placeholder = '', size = 'md' }: SearchInputProps) {
   return (
     <S.SearchContainer size={size}>
       <S.Icon>
-        <BsSearch size={15} />
+        <BsSearch size={size === 'sm' ? 12 : 15} />
       </S.Icon>
       <S.SearchInput placeholder={placeholder} />
     </S.SearchContainer>
