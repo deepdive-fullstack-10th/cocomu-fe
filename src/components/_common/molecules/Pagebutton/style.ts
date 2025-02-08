94628b7bd22589e8c3a2eae220c31b3f601b0324
HYJIN\isacc <isacc7224@gmail.com>
2025-02-09 07:25:18 +0900

    COC-136-feat(#32): PageButton 컴포넌트 구현

---
+++ src/components/_common/molecules/Pagebutton/style.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+const PageButtonsContainer = styled.div`
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  user-select: none;
+`;
+
+const Item = styled.div`
+  cursor: pointer;
+`;
+
+const S = {
+  PageButtonsContainer,
+  Item,
+};
+
+export default S;
