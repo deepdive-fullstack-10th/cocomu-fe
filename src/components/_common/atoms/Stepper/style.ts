91bd77948ce8b1ce9e0cb376533d582a065f3fe2
HYJIN\isacc <isacc7224@gmail.com>
2025-02-11 01:37:01 +0900

    COC-136-refactor(#32): Stepper컴포넌트, pageButton 컴포넌트 리팩토링

--- src/components/_common/atoms/Stepper/style.ts
+++ src/components/_common/atoms/Stepper/style.ts
@@ -1,17 +1,23 @@
 import styled from '@emotion/styled';
 
 const Stepper = styled.div<{ select?: boolean }>`
-  cursor: pointer;
-  border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
-  font: ${({ theme }) => theme.font.common.small};
-  color: ${({ theme }) => theme.color.gray[950]};
+
   width: 1rem;
   height: 1rem;
-  padding: 1rem;
+
+  font: ${({ theme }) => theme.font.common.small};
+  color: ${({ theme }) => theme.color.gray[950]};
   background: ${({ theme, select }) => (select ? theme.color.primary[100] : theme.color.gray[50])};
+
+  border-radius: 50%;
+
+  padding: 1rem;
+
+  cursor: pointer;
+
   &:hover {
     background: ${({ theme }) => theme.color.gray[200]};
   }
--- src/components/_common/molecules/Pagebutton/style.ts
+++ src/components/_common/molecules/Pagebutton/style.ts
@@ -1,15 +1,18 @@
 import styled from '@emotion/styled';
 
 const PageButtonsContainer = styled.div`
-  width: fit-content;
   display: flex;
   align-items: center;
+
+  width: fit-content;
+
   user-select: none;
 `;
 
 const Item = styled.div`
-  cursor: pointer;
   margin-left: 0.5rem;
+
+  cursor: pointer;
 `;
 
 const S = {
