25178cf85e5f8cd1fffeeef1816c60bb969763bb
HYJIN\isacc <isacc7224@gmail.com>
2025-02-11 16:55:09 +0900

    COC-145-refactor(#42): upload, border props 추가

--- src/components/_common/molecules/ProfileLabel/index.tsx
+++ src/components/_common/molecules/ProfileLabel/index.tsx
@@ -5,14 +5,18 @@ interface ProfileLabelProps {
   name: string;
   src?: string;
   size: ProfileLabelSize;
+  upload?: boolean;
+  border?: boolean;
 }
 
-export default function ProfileLabel({ name, src, size }: ProfileLabelProps) {
+export default function ProfileLabel({ name, src, size, upload, border }: ProfileLabelProps) {
   return (
     <S.ProfileLabelContainer>
       <ProfileImage
         size={size}
         src={src}
+        upload={upload}
+        border={border}
       />
       <S.ProfileLabelName size={size}>{name}</S.ProfileLabelName>
     </S.ProfileLabelContainer>
