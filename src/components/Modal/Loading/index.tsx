c97a8544a8125ee6b56c5947190fe0a2bf95d595
KimBeomJo <dongjun1315@naver.com>
2025-02-11 18:24:53 +0900

    COC-141-feat(#41): 로딩 모달 들어가는 label 추가

--- src/components/Modal/Loading/index.tsx
+++ src/components/Modal/Loading/index.tsx
@@ -1,16 +1,37 @@
 import LoadingSpinner from '@components/_common/atoms/LoadingSpinner';
 import S from './style';
 
-interface LoadingIndicatorModalProps {
-  descriptionLabel?: string;
-  mainLabel?: string;
+type movePage = 'problem' | 'feedback' | 'exit';
+
+interface loadingDetailContent {
+  description?: string;
+  label?: string;
 }
 
-export default function LoadingModal({ descriptionLabel, mainLabel }: LoadingIndicatorModalProps) {
+const loadingType: Record<movePage, loadingDetailContent> = {
+  problem: {
+    description: '스페이스 모집이 완료되었습니다.',
+    label: '문제 풀이',
+  },
+  feedback: {
+    description: '문제 풀이가 종료되었습니다.',
+    label: '피드백',
+  },
+  exit: {
+    description: '피드백이 종료되었습니다',
+    label: '종료',
+  },
+} as const;
+
+interface LoadingModalProps {
+  navigate: movePage;
+}
+
+export default function LoadingModal({ navigate = 'problem' }: LoadingModalProps) {
   return (
     <S.LoadingModalContainer>
-      <S.Description>{descriptionLabel}</S.Description>
-      <S.Instruction>{mainLabel} 페이지로 이동합니다.</S.Instruction>
+      <S.Description>{loadingType[navigate].description}</S.Description>
+      <S.Instruction>{loadingType[navigate].label} 페이지로 이동합니다.</S.Instruction>
       <LoadingSpinner />
     </S.LoadingModalContainer>
   );
