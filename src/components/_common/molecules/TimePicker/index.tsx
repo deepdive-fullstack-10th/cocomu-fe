a8aca9fff0ae5b5bbb9cc8310e93135318599874
HYJIN\isacc <isacc7224@gmail.com>
2025-02-11 14:53:27 +0900

    COC-132-refactor(#26): 휠 이벤트 WheelPicker 컴포넌트로 이동, 코드 컨벤션 규칙 맞춰서 리팩토링

--- src/components/_common/molecules/TimePicker/index.tsx
+++ src/components/_common/molecules/TimePicker/index.tsx
@@ -1,13 +1,7 @@
 import { useState } from 'react';
 import { generateHours, generateMinutes } from '@utils/timeUtils';
-import WheelPiker from '@components/_common/atoms/WheelPicker';
+import WheelPicker from '@components/_common/atoms/WheelPicker';
 import S from './style';
-
-type WheelProps = {
-  e: React.WheelEvent;
-  setter: React.Dispatch<React.SetStateAction<number>>;
-  maxLength: number;
-};
 
 export default function TimePicker() {
   const HOURS = generateHours(10);
@@ -16,30 +10,20 @@ export default function TimePicker() {
   const [hourIndex, setHourIndex] = useState<number>(0);
   const [minuteIndex, setMinuteIndex] = useState<number>(0);
 
-  const handleWheel = ({ e, setter, maxLength }: WheelProps) => {
-    if (e.deltaY < 0) {
-      setter((prev) => (prev - 1 + maxLength) % maxLength);
-    } else if (e.deltaY > 0) {
-      setter((prev) => (prev + 1) % maxLength);
-    }
-  };
-
   return (
     <S.PickerContainer>
-      <WheelPiker
+      <WheelPicker
         label='시간'
         items={HOURS}
         selectedIndex={hourIndex}
-        onWheel={(e) => handleWheel({ e, setter: setHourIndex, maxLength: HOURS.length })}
-        onClick={setHourIndex}
+        updateIndex={setHourIndex}
       />
       <S.Colon>:</S.Colon>
-      <WheelPiker
+      <WheelPicker
         label='분'
         items={MINUTES}
         selectedIndex={minuteIndex}
-        onWheel={(e) => handleWheel({ e, setter: setMinuteIndex, maxLength: MINUTES.length })}
-        onClick={setMinuteIndex}
+        updateIndex={setMinuteIndex}
       />
     </S.PickerContainer>
   );
