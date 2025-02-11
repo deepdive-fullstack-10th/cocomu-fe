a3073c91b9afe3cebd6087802b79b96feed30dd4
HYJIN\isacc <isacc7224@gmail.com>
2025-02-11 14:00:09 +0900

    COC-132-refactor(#26): TimePiker molecules, WhellPicker atom 구분, 시간, 분 설정 함수 utils로 따로 작성

---
+++ src/components/_common/molecules/TimePicker/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { generateHours, generateMinutes } from '@utils/tileUtils';
+import WheelPiker from '@components/_common/atoms/WheelPicker';
+import S from './style';
+
+type WheelProps = {
+  e: React.WheelEvent;
+  setter: React.Dispatch<React.SetStateAction<number>>;
+  maxLength: number;
+};
+
+export default function TimePicker() {
+  const HOURS = generateHours(10);
+  const MINUTES = generateMinutes(5);
+
+  const [hourIndex, setHourIndex] = useState<number>(0);
+  const [minuteIndex, setMinuteIndex] = useState<number>(0);
+
+  const handleWheel = ({ e, setter, maxLength }: WheelProps) => {
+    if (e.deltaY < 0) {
+      setter((prev) => (prev - 1 + maxLength) % maxLength);
+    } else if (e.deltaY > 0) {
+      setter((prev) => (prev + 1) % maxLength);
+    }
+  };
+
+  return (
+    <S.PickerContainer>
+      <WheelPiker
+        label='시간'
+        items={HOURS}
+        selectedIndex={hourIndex}
+        onWheel={(e) => handleWheel({ e, setter: setHourIndex, maxLength: HOURS.length })}
+        onClick={setHourIndex}
+      />
+      <S.Colon>:</S.Colon>
+      <WheelPiker
+        label='분'
+        items={MINUTES}
+        selectedIndex={minuteIndex}
+        onWheel={(e) => handleWheel({ e, setter: setMinuteIndex, maxLength: MINUTES.length })}
+        onClick={setMinuteIndex}
+      />
+    </S.PickerContainer>
+  );
+}
---
+++ src/components/_common/molecules/TimePicker/style.ts
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const PickerContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: fit-content;
+  min-width: 15rem;
+
+  background-color: ${({ theme }) => theme.color.gray[50]};
+  border: 1px solid ${({ theme }) => theme.color.gray[600]};
+  border-radius: 0.8rem;
+
+  padding: 1.5rem 1rem;
+
+  user-select: none;
+`;
+
+export const Colon = styled.div`
+  ${({ theme }) => theme.font.heading[200]};
+
+  margin-top: 2.4rem;
+`;
+
+const S = { PickerContainer, Colon };
+export default S;
