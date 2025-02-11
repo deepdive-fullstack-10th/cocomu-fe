84bbaa610b502e241913f374e366f2a436b19085
KimBeomJo <dongjun1315@naver.com>
2025-02-11 19:45:24 +0900

    COC-144-feat(#41): useNavigate 훅 사용에 따른 BrowserRouter 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import globalStyles from './styles/globalStyles';
 import { theme } from './styles/theme';
 
 createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <Global styles={globalStyles()} />
-    <App />
-  </ThemeProvider>,
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles()} />
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
 );
