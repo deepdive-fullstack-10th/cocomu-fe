36a7d4bb6f29d053f3ef27bd67ff6093e01f7e73
Seio924 <seio9241@gmail.com>
2025-02-13 15:03:52 +0900

    COC-149-feat(#52): main에 모달 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
 import ToastList from '@components/_common/molecules/ToastList';
+import BaseModal from '@components/Modal/BaseModal';
 import App from './App';
 import globalStyles from './styles/globalStyles';
 import { theme } from './styles/theme';
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles()} />
       <ToastList />
+      <BaseModal />
       <App />
     </ThemeProvider>
   </BrowserRouter>,
