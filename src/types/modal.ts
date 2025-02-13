a23fdcbce70ed50a5dafa7318542beb28a0b8abb
Seio924 <seio9241@gmail.com>
2025-02-13 15:46:24 +0900

    COC-149-feat(#52): disableOutsideClick 옵션 추가

--- src/types/modal.ts
+++ src/types/modal.ts
@@ -1,12 +1,19 @@
 import Waiting from '@components/Modal/Waiting';
 
-export const MODAL_COMPONENTS: Record<string, React.FC<{ onClose: () => void } & Record<string, unknown>>> = {
-  waiting: Waiting,
-};
-
 export interface WaitingProps extends Record<string, unknown> {
   label?: string;
   description?: string;
   navigateUrl?: string;
   onClose?: () => void;
 }
+
+interface ModalConfig<T> {
+  Component: React.FC<T>;
+  disableOutsideClick?: boolean;
+}
+
+export const MODAL_COMPONENTS: {
+  waiting: ModalConfig<WaitingProps>;
+} = {
+  waiting: { Component: Waiting, disableOutsideClick: true },
+};
