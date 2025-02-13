b569f3348ed0396203956a50adf0aa71b3d60f58
Seio924 <seio9241@gmail.com>
2025-02-13 19:50:41 +0900

    COC-154-feat(#60): QueryClient 및 Devtool 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,40 @@
 import { createRoot } from 'react-dom/client';
-import { Global, ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useToastStore } from '@stores/useToastStore';
 import { BrowserRouter } from 'react-router-dom';
+import { Global, ThemeProvider } from '@emotion/react';
 import ToastList from '@components/_common/molecules/ToastList';
 import BaseModal from '@components/Modal/BaseModal';
 import App from './App';
 import globalStyles from './styles/globalStyles';
 import { theme } from './styles/theme';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      retry: 0,
+    },
+    mutations: {
+      onError: (error) => {
+        const { error: alertError } = useToastStore.getState();
+        alertError(error.message);
+      },
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <Global styles={globalStyles()} />
-      <ToastList />
-      <BaseModal />
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyles()} />
+        <ToastList />
+        <BaseModal />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
