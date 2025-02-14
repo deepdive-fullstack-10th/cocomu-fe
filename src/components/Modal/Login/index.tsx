cfdfb7670f0f6bc80d12bf1763151cbcde5b7cf9
KimBeomJo <dongjun1315@naver.com>
2025-02-14 21:40:43 +0900

    COC-155-feat(#63): 이미지 삽입 및 프롭스 타입 설정

--- src/components/Modal/Login/index.tsx
+++ src/components/Modal/Login/index.tsx
@@ -1,29 +1,37 @@
 import { BsXLg } from 'react-icons/bs';
-import S from "./style";
+import { LoginProps } from '@customTypes/modal';
+import S from './style';
+import COCOMU_LOGO from '@/assets/Image/cocomu-logo.png';
+import GOOGLE_LOGO from '@/assets/Image/google-logo.png';
+import GITHUB_LOGO from '@/assets/Image/github-logo.png';
+import KAKAO_LOGO from '@/assets/Image/kakao-logo.png';
 
-export default Login({ onClick, onClose }) {
+export default function LoginModal({ onClose, onGoogle, onGitHub, onKakao }: LoginProps) {
   return (
-    <S.LoginModalContainter>
+    <S.LoginModalContainer>
       <S.CloseIcon>
-        <BsXLg size={10} onClick={onClose}/>
+        <BsXLg
+          size={10}
+          onClick={onClose}
+        />
       </S.CloseIcon>
       <S.LogoContainer>
-        <S.LOGO src={}></S.LOGO>
+        <S.Logo src={COCOMU_LOGO} />
       </S.LogoContainer>
       <S.IntroduceLabel>코코무에 오신 것을</S.IntroduceLabel>
-      <S.inroduceLabel>환영합니다!</S.inroduceLabel>
-      <S.GoogleLoginButton onClick={onClick}>
-        <S.ButtonIcon src={}></S.ButtonIcon>
+      <S.IntroduceLabel>환영합니다!</S.IntroduceLabel>
+      <S.GoogleLoginButton onClick={onGoogle}>
+        <S.ButtonIcon src={GOOGLE_LOGO} />
         <S.ButtonLabel>Google 로그인</S.ButtonLabel>
       </S.GoogleLoginButton>
-      <S.GitHubLoginButton onClick={onClick}>
-        <S.ButtonIcon></S.ButtonIcon>
+      <S.GitHubLoginButton onClick={onGitHub}>
+        <S.ButtonIcon src={GITHUB_LOGO} />
         <S.ButtonLabel>GitHub 로그인</S.ButtonLabel>
       </S.GitHubLoginButton>
-      <S.KakaoLoginButton onClick={onClick}>
-        <S.ButtonIcon></S.ButtonIcon>
+      <S.KakaoLoginButton onClick={onKakao}>
+        <S.ButtonIcon src={KAKAO_LOGO} />
         <S.ButtonLabel>Kakao 로그인</S.ButtonLabel>
       </S.KakaoLoginButton>
-    </S.LoginModalContainter>
+    </S.LoginModalContainer>
   );
 }
