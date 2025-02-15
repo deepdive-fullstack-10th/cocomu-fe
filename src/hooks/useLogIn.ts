ad886e5938ca430bb8199f36c841ba46a45a77ac
Seio924 <seio9241@gmail.com>
2025-02-16 04:11:11 +0900

    COC-154-feat(#60): 로그인 처리 훅 추가

---
+++ src/hooks/useLogIn.ts
@@ -0,0 +1,17 @@
+import { axiosInstance } from '@api/axiosInstance';
+import authApi from '@api/domain/auth';
+import { ACCESS_TOKEN_KEY } from '@constants/api';
+import { useMutation } from '@tanstack/react-query';
+
+export default function useLogIn() {
+  const logInMutate = useMutation({
+    mutationFn: authApi.login,
+    onSuccess: ({ accessToken }) => {
+      localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+      axiosInstance.defaults.headers.Authorization = `Bearer ${accessToken}`;
+    },
+  });
+  return {
+    logInMutate,
+  };
+}
