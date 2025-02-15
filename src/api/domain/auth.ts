c7047bfe3339c55c7ae9c9e7281a2aa84140008b
Seio924 <seio9241@gmail.com>
2025-02-16 04:04:16 +0900

    COC-154-feat(#60): OAuth 로그인 API 호출 함수 구현

---
+++ src/api/domain/auth.ts
@@ -0,0 +1,22 @@
+import { axiosInstance } from '@api/axiosInstance';
+import { END_POINTS_V1 } from '@constants/api';
+import { TokenData } from '@customTypes/auth';
+
+interface LogInParams {
+  provider: string;
+  oauthCode: number;
+}
+
+const authApi = {
+  login: async ({ provider, oauthCode }: LogInParams) => {
+    const { data } = await axiosInstance.post<TokenData>(
+      END_POINTS_V1.AUTH.OAUTH_LOGIN,
+      { params: { provider, oauthCode } },
+      { useAuth: false },
+    );
+
+    return data;
+  },
+};
+
+export default authApi;
