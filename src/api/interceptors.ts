0e01ed5a7c379363c90344a066d31ce5bc8787a1
Seio924 <seio9241@gmail.com>
2025-02-16 03:57:22 +0900

    COC-154-feat(#60): 응답 인터셉터 개선
    - handleSuccess 적용하여 result 자동 추출

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,4 +1,4 @@
-import type { AxiosError, InternalAxiosRequestConfig } from 'axios';
+import type { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 import { HTTPError } from '@api/HTTPError';
 import { axiosInstance } from '@api/axiosInstance';
@@ -65,3 +65,5 @@ export const handleAPIError = (error: AxiosError<ErrorResponseData>) => {
 
   throw new HTTPError(status, data.message, data.code);
 };
+
+export const handleSuccess = (response: AxiosResponse) => response.data;
