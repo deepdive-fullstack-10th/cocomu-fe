a14d04f19f9999240dd01bf0de54ea3937ff296b
KimBeomJo <dongjun1315@naver.com>
2025-02-15 15:19:31 +0900

    COC-152-feat(#56): input 드롭다운 입력 기능 추가

--- src/components/_common/molecules/SelectDropdown/index.tsx
+++ src/components/_common/molecules/SelectDropdown/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import DropdownItem from '@components/_common/atoms/DropdownItem';
 import { STEP_INFO } from '@constants/constants';
 import { DropDownItemStyleProps } from '@components/_common/atoms/DropdownItem/style';
+import Icon from '@components/_common/atoms/Icon';
 import S from './style';
 
 type StepLabel = (typeof STEP_INFO)[keyof typeof STEP_INFO]['label'];
@@ -11,7 +12,7 @@ type Dropdown2Props = {
   items: StepLabel[];
 } & DropDownItemStyleProps;
 
-export default function Dropdown2({ items, color, size }: Dropdown2Props & DropDownItemStyleProps) {
+export default function SelectDropdown({ items, color, size }: Dropdown2Props & DropDownItemStyleProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState<StepLabel>(items[0]);
 
@@ -29,7 +30,10 @@ export default function Dropdown2({ items, color, size }: Dropdown2Props & DropD
       <S.Dropdown2Header onClick={() => toggleDropdown()}>
         <S.DropdownText>{selectedItem}</S.DropdownText>
         <S.IconContainer>
-          <BsChevronDown size={10} />
+          <Icon
+            icon={<BsChevronDown size={10} />}
+            color='950'
+          />
         </S.IconContainer>
       </S.Dropdown2Header>
       <S.DropdownItemContainer isOpen={isOpen}>
