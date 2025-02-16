54f510194a4486d46c6899f6da70808d00be0d5e
Re-Note <dldydtn207@gmail.com>
2025-02-17 08:16:31 +0900

    COC-157-feat(#69): Confirm Modal 컴포넌트 구현

---
+++ src/components/Modal/Confirm/style.ts
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem 6.75rem;
+  background: white;
+  border-radius: 1.2rem;
+  width: 52rem;
+  height: 30.8rem;
+`;
+
+const Description = styled.h2`
+  ${({ theme }) => theme.font.heading[300]};
+  color: ${({ theme }) => theme.color.gray[600]};
+  margin-bottom: 1.6rem;
+`;
+
+const Instruction = styled.h1`
+  ${({ theme }) => theme.font.heading[500]};
+  color: ${({ theme }) => theme.color.gray[950]};
+  margin-bottom: 3.2rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-self: center;
+  justify-content: flex-start;
+  gap: 1rem;
+  margin-top: 1.8rem;
+  height: 4.2rem;
+`;
+
+const S = { Container, Description, Instruction, ButtonContainer };
+export default S;
