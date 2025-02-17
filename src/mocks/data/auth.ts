67b1b49a03ad1499944a6134e57835ddde522afd
Seio924 <seio9241@gmail.com>
2025-02-17 15:29:08 +0900

    COC-154-feat(#60): 토큰 재발급 mock 추가

--- src/mocks/data/auth.ts
+++ src/mocks/data/auth.ts
@@ -12,3 +12,11 @@ export const loginErrorResponse = {
   code: 1001,
   message: '로그인에 실패했습니다.',
 };
+
+export const reIssueResponse = {
+  code: 1000,
+  message: '새로운 토큰 발급에 성공했습니다.',
+  result: {
+    accessToken: 'cocomuAccessToken',
+  },
+};
--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -1,9 +1,9 @@
 import { http, HttpResponse } from 'msw';
 import { loginErrorResponse, loginResponse, refreshToken } from '@mocks/data/auth';
-import { END_POINTS_V1, HTTP_STATUS_CODE } from '@constants/api';
+import { BASE_URL, END_POINTS_V1, HTTP_STATUS_CODE } from '@constants/api';
 
 export const authHandlers = [
-  http.post(`http://localhost:8080${END_POINTS_V1.AUTH.OAUTH_LOGIN}`, async ({ request }) => {
+  http.post(`${BASE_URL}${END_POINTS_V1.AUTH.OAUTH_LOGIN}`, async ({ request }) => {
     const url = new URL(request.url);
     const provider = url.searchParams.get('provider');
     const oauthCode = url.searchParams.get('oauthCode');
@@ -22,4 +22,16 @@ export const authHandlers = [
       },
     });
   }),
+
+  http.post(
+    `${BASE_URL}${END_POINTS_V1.AUTH.REFRESH_TOKEN}`,
+    async () =>
+      new HttpResponse(JSON.stringify(loginResponse), {
+        status: HTTP_STATUS_CODE.SUCCESS,
+        headers: {
+          'Set-Cookie': `refreshToken=${refreshToken}; HttpOnly; Path=/`,
+          'Content-Type': 'application/json',
+        },
+      }),
+  ),
 ];
