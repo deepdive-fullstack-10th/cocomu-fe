d61652a5a08f4d7edcf54b52f5d2a734bc9fd033
KimBeomJo <dongjun1315@naver.com>
2025-02-21 19:39:58 +0900

    COC-164-feat(#75): useError 커스텀 훅 분리

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,40 +1,6 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
-
-interface UseFieldErrors<T> {
-  errors: Partial<Record<keyof T, string>>;
-  setError: (field: keyof T, message: string) => void;
-  clearError: (field: keyof T) => void;
-  clearAllErrors: () => void;
-  hasErrors: boolean;
-}
-
-export function useFieldErrors<T>(): UseFieldErrors<T> {
-  const [errors, setErrors] = useState<Partial<Record<keyof T, string>>>({});
-
-  const setError = (field: keyof T, message: string) => {
-    setErrors((prev) => ({ ...prev, [field]: message }));
-  };
-
-  const clearError = (field: keyof T) => {
-    setErrors((prev) => {
-      const next = { ...prev };
-      delete next[field];
-      return next;
-    });
-  };
-
-  const clearAllErrors = () => {
-    setErrors({});
-  };
-
-  return {
-    errors,
-    setError,
-    clearError,
-    clearAllErrors,
-    hasErrors: Object.keys(errors).length > 0,
-  };
-}
+import { FieldValidator, ValidationRules } from '@utils/validation';
+import { useFieldErrors } from '@hooks/useError';
 
 interface UseFormProps<T> {
   initialValues: T;
