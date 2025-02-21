c3c7f9a8ef079c510863e7fba6ebb9d8f82c9510
KimBeomJo <dongjun1315@naver.com>
2025-02-21 19:36:55 +0900

    COC-164-feat(#75): 검증 클래스 분리

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,30 +1,5 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 
-export class ValidationResult {
-  constructor(
-    public isValid: boolean,
-    public message?: string,
-  ) {}
-
-  static success() {
-    return new ValidationResult(true);
-  }
-
-  static failure(message: string) {
-    return new ValidationResult(false, message);
-  }
-}
-
-export interface FieldValidator {
-  onChange?: (value: string) => ValidationResult;
-  onBlur?: (value: string) => ValidationResult;
-  onSubmit?: (value: string) => ValidationResult;
-}
-
-export interface ValidationRules {
-  [field: string]: FieldValidator;
-}
-
 interface UseFieldErrors<T> {
   errors: Partial<Record<keyof T, string>>;
   setError: (field: keyof T, message: string) => void;
