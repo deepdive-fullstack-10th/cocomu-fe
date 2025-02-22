8ead5119469e5be5bd1e3d752e7b16dcffefc6b3
Seio924 <seio9241@gmail.com>
2025-02-22 20:07:08 +0900

    COC-166-refactor(#76): 암호 입력 모달 리팩토링

--- src/components/Modal/PasswordInput/style.ts
+++ src/components/Modal/PasswordInput/style.ts
@@ -3,30 +3,25 @@ import styled from '@emotion/styled';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  padding: 4rem 6.75rem;
-  background: white;
-  border-radius: 1.2rem;
+  gap: 3.3rem;
+
+  padding: 6.6rem 6.75rem 5.4rem 6.75rem;
   width: 52rem;
-  height: 31.7rem;
 `;
 
 const Description = styled.h2`
   ${({ theme }) => theme.font.heading[400]};
   color: ${({ theme }) => theme.color.gray[950]};
-  margin-bottom: 3.2rem;
 `;
 
-const ButtonContainer = styled.div`
+const ButtonWrapper = styled.div`
   display: flex;
-  justify-content: flex-end;
   align-self: flex-end;
   gap: 1rem;
-  margin-top: 1.8rem;
-  width: 21.5rem;
+
   height: 4.2rem;
 `;
 
-const S = { Container, Description, ButtonContainer };
+const S = { Container, Description, ButtonWrapper };
 export default S;
