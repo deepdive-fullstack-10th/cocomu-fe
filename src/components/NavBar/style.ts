3bd04298bd524fb534eaa204b18beef35840c079
Seio924 <seio9241@gmail.com>
2025-02-23 16:07:41 +0900

    COC-166-refactor(#76): 네브바 리팩토링

--- src/components/NavBar/style.ts
+++ src/components/NavBar/style.ts
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
 
-const NavbarContainer = styled.nav`
+const Container = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
   padding: 1rem 10rem;
 `;
 
@@ -16,19 +17,19 @@ const NavItems = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 21rem;
+  gap: 3.8rem;
 `;
 
 const LoginButton = styled.button`
-  width: 8rem;
   color: ${({ theme }) => theme.color.gray[950]};
-  ${({ theme }) => theme.font.common.default};
+  ${({ theme }) => theme.font.heading[100]};
 `;
 
 const ProfileSection = styled.div`
+  position: relative;
+
   display: flex;
   align-items: center;
-  position: relative;
   gap: 0.5rem;
 `;
 
@@ -36,25 +37,19 @@ const DropdownMenu = styled.div`
   position: absolute;
   right: 0;
   top: 100%;
+
   background-color: ${({ theme }) => theme.color.gray[50]};
   border: 1px solid ${({ theme }) => theme.color.gray[200]};
   border-radius: 0.5rem;
 `;
 
-const IconWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-`;
-
 const S = {
-  NavbarContainer,
+  Container,
   LogoImage,
   NavItems,
   LoginButton,
   ProfileSection,
   DropdownMenu,
-  IconWrapper,
 };
 
 export default S;
