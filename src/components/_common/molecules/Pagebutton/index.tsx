e23cc4d4df38162ee8b38230a8468812e5b8ca5d
Re-Note <dldydtn207@gmail.com>
2025-02-24 17:20:20 +0900

    COC-170-refactor(#80): PageButton 수정

--- src/components/_common/molecules/Pagebutton/index.tsx
+++ src/components/_common/molecules/Pagebutton/index.tsx
@@ -2,14 +2,16 @@ import Pagination from '@mui/material/Pagination';
 
 interface PageButtonsProps {
   pages: number;
+  onPageChange: (event: React.ChangeEvent<unknown>, page: number) => void;
 }
 
-export default function PageButton({ pages }: PageButtonsProps) {
+export default function PageButton({ pages, onPageChange }: PageButtonsProps) {
   return (
     <Pagination
       count={pages}
       color='primary'
       size='small'
+      onChange={onPageChange}
       sx={{
         '& .MuiPaginationItem-root': {
           typography: 'common.small',
