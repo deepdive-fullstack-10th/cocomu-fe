47755bbd49b585da61945e7e770ff5e76ba712e8
Seio924 <seio9241@gmail.com>
2025-02-25 20:09:32 +0900

    COC-86-feat(#82): 공개, 비공개 스터디 mock 데이터 설정

---
+++ src/mocks/data/study.ts
@@ -0,0 +1,15 @@
+export const createPublicResponse = {
+  code: 1200,
+  message: '공개 스터디 생성에 성공했습니다.',
+  result: {
+    studyId: 1,
+  },
+};
+
+export const createPrivateResponse = {
+  code: 1200,
+  message: '비공개 스터디 생성에 성공했습니다.',
+  result: {
+    studyId: 1,
+  },
+};
