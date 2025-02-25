21af8be1a7f16273f805dc7a09ff2fb4742e99e6
Seio924 <seio9241@gmail.com>
2025-02-25 12:54:23 +0900

    COC-86-feat(#82): MainLayout 생성

--- src/pages/MainLayout/index.tsx
+++ src/pages/MainLayout/index.tsx
@@ -1,5 +1,18 @@
+import NavBar from '@components/NavBar';
+import { NAVBAR_DROPDOWN_LABELS } from '@constants/constants';
 import { Outlet } from 'react-router-dom';
+import S from './style';
 
 export default function MainLayout() {
-  return <Outlet />;
+  return (
+    <S.MainContainer>
+      <NavBar
+        items={NAVBAR_DROPDOWN_LABELS}
+        isLogined={false}
+      />
+      <S.Container>
+        <Outlet />
+      </S.Container>
+    </S.MainContainer>
+  );
 }
---
+++ src/pages/MainLayout/style.ts
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+const S = {
+  MainContainer,
+  Container,
+};
+
+export default S;
