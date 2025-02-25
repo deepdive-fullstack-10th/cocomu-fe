ebb36a66e89d41767e4bd94d50bfae56e6220540
Seio924 <seio9241@gmail.com>
2025-02-25 12:53:55 +0900

    COC-86-chore(#82): react-quill-new로 업데이트

--- src/components/_common/atoms/TextEditor/index.tsx
+++ src/components/_common/atoms/TextEditor/index.tsx
@@ -1,6 +1,6 @@
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import 'react-quill/dist/quill.bubble.css';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
+import 'react-quill-new/dist/quill.bubble.css';
 import './style.css';
 
 interface TextEditorProps {
