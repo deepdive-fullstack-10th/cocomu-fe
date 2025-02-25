61e024d233cd61a8a0729d07d1b533b819935b28
Seio924 <seio9241@gmail.com>
2025-02-25 10:48:17 +0900

    COC-86-feat(#82): input Section 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
-  return (
-    <div>
-      <h1>App</h1>
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 }
