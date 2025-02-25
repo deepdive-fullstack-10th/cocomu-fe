a85239ce69a33b5feb3228485a6436eb48a1f022
Re-Note <dldydtn207@gmail.com>
2025-02-25 11:49:30 +0900

    COC-91-feat(#73): StudyList footer 레이아웃 구현

---
+++ src/pages/Study/StudyList/footer/style.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 6.6rem 0rem;
+`;
--- src/pages/Study/StudyList/index.tsx
+++ src/pages/Study/StudyList/index.tsx
@@ -1,10 +1,15 @@
 import Body from './body';
+import Footer from './footer';
 import * as S from './style';
 
 export default function StudyList() {
   return (
     <S.Container>
       <Body />
+      <Footer
+        pages={10}
+        onPageChange={() => {}}
+      />
     </S.Container>
   );
 }
