8b55314475944af2ab50c00602ab344c721426ee
Re-Note <dldydtn207@gmail.com>
2025-02-25 13:09:10 +0900

    COC-91-feat(#73): MainLayout 구현 추가

--- src/pages/Study/StudyList/style.ts
+++ src/pages/Study/StudyList/style.ts
@@ -3,4 +3,7 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 60px 16px;
 `;
