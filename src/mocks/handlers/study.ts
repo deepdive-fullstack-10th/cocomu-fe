b84c7cd8d2ae0feea483144071c5906e866e443d
Seio924 <seio9241@gmail.com>
2025-02-26 19:42:50 +0900

    COC-171-feat(#86): 스터디 수정 mock api 핸들러 추가

--- src/mocks/handlers/study.ts
+++ src/mocks/handlers/study.ts
@@ -4,10 +4,12 @@ import {
   createPrivateResponse,
   createPublicResponse,
   createStudyErrorResponse,
+  editStudyErrorResponse,
+  editStudyResponse,
   getStudyInfoErrorResponse,
   getStudyInfoResponse,
 } from '@mocks/data/study';
-import { CreateStudyData } from '@customTypes/study';
+import { CreateStudyData, EditStudyData } from '@customTypes/study';
 
 export const studyHandlers = [
   http.get(`${BASE_URL}${END_POINTS_V1.STUDY.INFO(':studyId')}`, async ({ params }) => {
@@ -47,6 +49,28 @@ export const studyHandlers = [
       });
     }
     return new HttpResponse(JSON.stringify(createPrivateResponse), {
+      status: HTTP_STATUS_CODE.SUCCESS,
+    });
+  }),
+
+  http.post(`${BASE_URL}${END_POINTS_V1.STUDY.EDIT(':studyId')}`, async ({ request, params }) => {
+    const { studyId } = params;
+    const body = (await request.json()) as EditStudyData;
+
+    if (
+      !studyId ||
+      !body.status ||
+      !body.name ||
+      !body.totalUserCount ||
+      !body.languages ||
+      !body.judges ||
+      !body.description
+    ) {
+      return new HttpResponse(JSON.stringify(editStudyErrorResponse), {
+        status: HTTP_STATUS_CODE.BAD_REQUEST,
+      });
+    }
+    return new HttpResponse(JSON.stringify(editStudyResponse), {
       status: HTTP_STATUS_CODE.SUCCESS,
     });
   }),
