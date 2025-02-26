1e85b908f114104c241e7034d67b0941901a2d12
Seio924 <seio9241@gmail.com>
2025-02-26 19:59:33 +0900

    COC-171-feat(#86): 스터디 정보 갱신을 위한 쿼리 무효화 추가

--- src/hooks/useEditStudy.ts
+++ src/hooks/useEditStudy.ts
@@ -1,16 +1,19 @@
 import studyApi from '@api/domain/study';
 import { ROUTES } from '@constants/path';
+import QUERY_KEYS from '@constants/queryKeys';
 import { EditStudyData } from '@customTypes/study';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 export default function useEditStudy() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const editStudyMutate = useMutation({
     mutationFn: ({ studyId, editStudyData }: { studyId: string; editStudyData: EditStudyData }) =>
       studyApi.editStudy(studyId, editStudyData),
     onSuccess: ({ studyId }) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.STUDY_DETAIL, studyId] });
       navigate(ROUTES.STUDY.PARTICIPATION({ studyId }));
     },
   });
