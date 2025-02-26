61c91066ff8501e38b694dfc6302e0e054e8a25b
KimBeomJo <dongjun1315@naver.com>
2025-02-26 14:50:57 +0900

    COC-83-feat(#79): 목 데이터 생성 및 핸들러 함수 구현
    # Conflicts:
    #	src/mocks/handlers/space.ts
    
    # Conflicts:
    #	src/mocks/handlers/space.ts

--- src/mocks/handlers/space.ts
+++ src/mocks/handlers/space.ts
@@ -8,6 +8,7 @@ import {
   TabData,
 } from '@mocks/data/space';
 import { BASE_URL, END_POINTS_V1, HTTP_STATUS_CODE } from '@constants/api';
+import { getSpaceListErrorResponse, getSpaceListResponse } from '@mocks/data/space';
 import { CreateSpaceData } from '@customTypes/space';
 
 export const spaceDetailHandlers = [
@@ -66,6 +67,22 @@ export const tabDataHandlers = [
 ];
 
 export const spaceHandlers = [
+  http.get(`${END_POINTS_V1.STUDY.SPACE_LIST(':studyId')}`, ({ params }) => {
+    const { studyId } = params;
+
+    if (!studyId || Number.isNaN(Number(studyId))) {
+      return new HttpResponse(JSON.stringify(getSpaceListErrorResponse), {
+        status: HTTP_STATUS_CODE.BAD_REQUEST,
+      });
+    }
+
+    return new HttpResponse(JSON.stringify(getSpaceListResponse), {
+      status: HTTP_STATUS_CODE.SUCCESS,
+    });
+  }),
+];
+
+export const spaceCreateHandlers = [
   http.post(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.CREATE}`, async ({ request }) => {
     const body = (await request.json()) as CreateSpaceData;
 
