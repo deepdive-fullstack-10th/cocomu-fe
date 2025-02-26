58c4d310d980d52759e36c9c1e6215490e91f20e
KimBeomJo <dongjun1315@naver.com>
2025-02-26 17:53:28 +0900

    COC-83-fix(#79): 엔드 포인트 수정 및 에러 해결

--- src/mocks/handlers/space.ts
+++ src/mocks/handlers/space.ts
@@ -1,9 +1,9 @@
 import { http, HttpResponse } from 'msw';
-import { END_POINTS_V1, HTTP_STATUS_CODE } from '@constants/api';
+import { BASE_URL, END_POINTS_V1, HTTP_STATUS_CODE } from '@constants/api';
 import { getSpaceListErrorResponse, getSpaceListResponse } from '@mocks/data/space';
 
 export const spaceHandlers = [
-  http.get(`${END_POINTS_V1.STUDY.SPACE_LIST(':studyId')}`, ({ params }) => {
+  http.get(`${BASE_URL}${END_POINTS_V1.STUDY.SPACE_LIST(':studyId')}`, ({ params }) => {
     const { studyId } = params;
 
     if (!studyId || Number.isNaN(Number(studyId))) {
