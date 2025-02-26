f30ca01612379636531afcca73022d9262dad00f
Seio924 <seio9241@gmail.com>
2025-02-26 19:42:01 +0900

    COC-171-feat(#86): 스터디 수정 mock 데이터 설정

--- src/mocks/data/study.ts
+++ src/mocks/data/study.ts
@@ -44,3 +44,16 @@ export const createStudyErrorResponse = {
   code: 4200,
   message: '유효성 검사 실패',
 };
+
+export const editStudyResponse = {
+  code: 1200,
+  message: '스터디 수정에 성공했습니다.',
+  result: {
+    studyId: 1,
+  },
+};
+
+export const editStudyErrorResponse = {
+  code: 4200,
+  message: '유효성 검사 실패',
+};
