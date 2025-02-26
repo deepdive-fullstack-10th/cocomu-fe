1eb61717c1a16b29304cf803ad3c5aab140f136d
Seio924 <seio9241@gmail.com>
2025-02-26 15:35:05 +0900

    COC-171-feat(#86): 스터디 정보 조회 mock 데이터 설정

--- src/mocks/data/study.ts
+++ src/mocks/data/study.ts
@@ -1,3 +1,29 @@
+export const getStudyInfoResponse = {
+  code: 1100,
+  message: '스터디 정보 조회에 성공하였습니다.',
+  result: {
+    id: 1,
+    joinable: true,
+    name: '딥다이브 스터디',
+    status: 'PUBLIC',
+    languages: ['Python'],
+    judges: ['백준'],
+    description: '<p>스터디 소개임</p>',
+    totalUserCount: 7,
+    createdAt: '2025-02-01T13:20:50.197Z',
+    leader: {
+      id: 1,
+      name: '새싹이',
+      profileImageUrl: 'https://cdn.cocomu.co.kr/images/default/profile.png',
+    },
+  },
+};
+
+export const getStudyInfoErrorResponse = {
+  code: 4200,
+  message: '스터디 정보 조회에 실패했습니다.',
+};
+
 export const createPublicResponse = {
   code: 1200,
   message: '공개 스터디 생성에 성공했습니다.',
