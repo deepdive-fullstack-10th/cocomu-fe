4d9109abc0afed1f251be6f9caea532d7ace2c7f
HYJIN\isacc <isacc7224@gmail.com>
2025-02-27 10:13:36 +0900

    COC-62-feat(#89): 스페이스 running페이지 Layout 구현

--- src/pages/Space/SpaceRunning/index.tsx
+++ src/pages/Space/SpaceRunning/index.tsx
@@ -1,3 +1,47 @@
+import { useDraggable } from '@hooks/useDraggable';
+import Tab from '@components/_common/atoms/Tab';
+// import * as monaco from 'monaco-editor';
+import MonacoEditor from '@monaco-editor/react';
+import SpaceRunner from '../SpaceRunner';
+import S from './style';
+
 export default function SpaceStart() {
-  return <div>SpaceStart</div>;
+  const {
+    value: height,
+    containerRef,
+    handleMouseDown,
+  } = useDraggable({
+    direction: 'y',
+    initialValue: 70,
+    min: 10,
+    max: 90,
+    threshold: 5,
+  });
+  return (
+    <S.Container ref={containerRef}>
+      <S.CodingContainer height={height}>
+        <S.TabContainer>
+          <Tab
+            color='primary'
+            name='참여자'
+          />
+        </S.TabContainer>
+        <MonacoEditor
+          defaultLanguage='javascript'
+          theme='vs-dark'
+          options={{
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
+            automaticLayout: true,
+          }}
+        />
+      </S.CodingContainer>
+      <S.ResizablePanel>
+        <S.ResizeButton onMouseDown={handleMouseDown} />
+      </S.ResizablePanel>
+      <S.RunnerContainer>
+        <SpaceRunner />
+      </S.RunnerContainer>
+    </S.Container>
+  );
 }
