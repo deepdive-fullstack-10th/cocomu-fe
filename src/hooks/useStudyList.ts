e48c816d474408c975ab99a7ce616d00d2a94f6f
Re-Note <dldydtn207@gmail.com>
2025-02-28 11:57:07 +0900

    COC-91-refactor(#73): 스터디 목록 조회 hook setQueryData 적용

--- src/hooks/useStudyList.ts
+++ src/hooks/useStudyList.ts
@@ -1,7 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { FetchStudiesData } from '@customTypes/study';
 import studyApi from '@api/domain/study';
 import QUERY_KEYS from '@constants/queryKeys';
+import { useEffect } from 'react';
 
 interface UseStudyListProps {
   queryParams: FetchStudiesData;
@@ -9,15 +10,25 @@ interface UseStudyListProps {
 }
 
 export default function useStudyList({ queryParams, onTotalItemsChange }: UseStudyListProps) {
+  const queryClient = useQueryClient();
   const { page } = queryParams;
 
-  return useQuery({
+  const query = useQuery({
     queryKey: [QUERY_KEYS.STUDY_LIST, queryParams],
     queryFn: async () => {
       const { studies, totalPage } = await studyApi.fetchStudies(queryParams);
-      onTotalItemsChange(totalPage);
+      queryClient.setQueryData([QUERY_KEYS.STUDY_LIST, 'totalPage'], totalPage);
       return studies;
     },
     enabled: page !== undefined,
   });
+
+  useEffect(() => {
+    const totalPage = queryClient.getQueryData<number>([QUERY_KEYS.STUDY_LIST, 'totalPage']);
+    if (totalPage !== undefined) {
+      onTotalItemsChange(totalPage);
+    }
+  }, [query.data, queryClient, onTotalItemsChange]);
+
+  return query;
 }
