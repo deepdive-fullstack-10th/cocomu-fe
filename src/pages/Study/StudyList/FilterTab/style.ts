1872c65b00cb479ac4741df8a57448670e5ca4b8
Re-Note <dldydtn207@gmail.com>
2025-02-28 12:25:20 +0900

    COC-91-refactor(#73): StudyList 폴더 구조 수정, 이름 수정

--- src/pages/Study/StudyList/FilterTab/style.ts
+++ src/pages/Study/StudyList/FilterTab/style.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const HeaderContainer = styled.div`
+export const FilterTabContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
