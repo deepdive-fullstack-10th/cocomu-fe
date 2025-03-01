d0f26fe8b18a4fd4886e8d83370689e9d1ad8d4a
Seio924 <seio9241@gmail.com>
2025-03-01 22:53:58 +0900

    COC-97-refactor(#90): PageButton 리팩토링

--- src/components/_common/molecules/PageButton/index.tsx
+++ src/components/_common/molecules/PageButton/index.tsx
@@ -1,17 +1,54 @@
-import { StyledPagination } from './style';
+import { PaginationItem } from '@mui/material';
+import Icon from '@components/_common/atoms/Icon';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+import S from './style';
 
 interface PageButtonsProps {
   pages: number;
   onPageChange: (event: React.ChangeEvent<unknown>, page: number) => void;
 }
 
+function PreviousButton() {
+  return (
+    <S.PaginationNavButton>
+      <Icon
+        size='sm'
+        color='950'
+      >
+        <BsChevronLeft />
+      </Icon>
+      이전
+    </S.PaginationNavButton>
+  );
+}
+
+function NextButton() {
+  return (
+    <S.PaginationNavButton>
+      다음
+      <Icon
+        size='sm'
+        color='950'
+      >
+        <BsChevronRight />
+      </Icon>
+    </S.PaginationNavButton>
+  );
+}
+
 export default function PageButton({ pages, onPageChange }: PageButtonsProps) {
   return (
-    <StyledPagination
+    <S.StyledPagination
       count={pages}
       color='primary'
       size='small'
       onChange={onPageChange}
+      renderItem={(item) => (
+        <PaginationItem
+          {...item}
+          slots={{ previous: PreviousButton, next: NextButton }}
+        />
+      )}
     />
   );
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,23 +2,10 @@ import { Theme } from '@emotion/react';
 import { createTheme } from '@mui/material/styles';
 import { dark, light } from '@styles/color';
 import typography from '@styles/font';
-import { muiTypography } from './muiTypography';
 
 const mode: 'light' | 'dark' = 'light';
 
-const muiTheme = createTheme({
-  palette: {
-    mode,
-    primary: {
-      main: mode === 'light' ? light.primary[100] : dark.primary[100],
-    },
-    background: {
-      default: mode === 'light' ? light.gray[50] : dark.gray[950],
-      paper: mode === 'light' ? light.gray[200] : dark.gray[900],
-    },
-  },
-  typography: muiTypography,
-});
+const muiTheme = createTheme();
 
 export const theme: Theme = {
   ...muiTheme,
