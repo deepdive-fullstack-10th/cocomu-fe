932ade4e46f2b0da54c4f69db4897eb4af30de1a
Seio924 <seio9241@gmail.com>
2025-03-01 21:36:55 +0900

    COC-97-feat(#90): 스페이스 생성 API 호출 함수 구현

--- src/api/domain/codingSpace.ts
+++ src/api/domain/codingSpace.ts
@@ -1,17 +1,26 @@
 import { axiosInstance } from '@api/axiosInstance';
 import { END_POINTS_V1 } from '@constants/api';
+import { CreateSpaceData } from '@customTypes/space';
 
 const spaceApi = {
+  create: async (createSpaceData: CreateSpaceData) => {
+    const { data } = await axiosInstance.post(END_POINTS_V1.CODING_SPACE.CREATE, createSpaceData);
+
+    return data.result;
+  },
+
   detail: async (codingSpaceId: string) => {
     const { data } = await axiosInstance.get(END_POINTS_V1.CODING_SPACE.PAGE(codingSpaceId));
 
     return data.result;
   },
+
   start: async ({ spaceId, studyId }: { spaceId: string; studyId: string }) => {
     await axiosInstance.post(END_POINTS_V1.CODING_SPACE.START(spaceId), { studyId });
 
     return spaceId;
   },
+
   tab: async (codingSpaceId: string) => {
     const { data } = await axiosInstance.get(END_POINTS_V1.CODING_SPACE.TAB(codingSpaceId));
 
