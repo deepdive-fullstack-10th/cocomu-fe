8de0e3e67e9b25f026cdb7bcdf9eef1c9f036ea3
Seio924 <seio9241@gmail.com>
2025-03-01 22:53:58 +0900

    COC-97-refactor(#90): PageButton 리팩토링

--- src/components/_common/molecules/PageButton/style.ts
+++ src/components/_common/molecules/PageButton/style.ts
@@ -1,10 +1,37 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Pagination from '@mui/material/Pagination';
 
-export const StyledPagination = styled(Pagination)`
-  user-select: none;
+const StyledPagination = styled(Pagination)(({ theme }) => ({
+  '& .MuiPaginationItem-root': css`
+    ${theme.font.common.block};
+    color: ${theme.color.gray[950]};
+    background-color: ${theme.color.gray[50]};
+    margin: 0 5px;
 
-  & .MuiPaginationItem-root {
-    typography: common.small;
-  }
+    &:hover {
+      background-color: ${theme.color.gray[200]};
+    }
+
+    &.Mui-selected {
+      color: ${theme.color.gray[950]};
+      background-color: ${theme.color.primary[200]};
+    }
+  `,
+}));
+
+const PaginationNavButton = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+
+  ${({ theme }) => theme.font.common.block};
+  color: ${({ theme }) => theme.color.gray[950]};
 `;
+
+const S = {
+  StyledPagination,
+  PaginationNavButton,
+};
+
+export default S;
