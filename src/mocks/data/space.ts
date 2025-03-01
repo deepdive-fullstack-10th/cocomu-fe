401745bfc743efa01dbcf5ff4ad1446ac57bab88
Seio924 <seio9241@gmail.com>
2025-03-01 21:38:30 +0900

    COC-97-feat(#90): 스페이스 생성 mock 데이터 설정

--- src/mocks/data/space.ts
+++ src/mocks/data/space.ts
@@ -122,3 +122,16 @@ export const TabData = {
     },
   },
 };
+
+export const createResponse = {
+  code: 1200,
+  message: '코딩 스페이스 생성에 성공했습니다.',
+  result: {
+    codingSpaceId: 1,
+  },
+};
+
+export const createErrorResponse = {
+  code: 4200,
+  message: '유효성 검사 실패',
+};
