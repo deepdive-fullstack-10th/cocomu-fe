a1c94e78340354689172f7f5cf52963b01c31e3c
Seio924 <seio9241@gmail.com>
2025-03-02 22:00:47 +0900

    COC-172-refactor(#97): 핸들러 책임 분리

--- src/pages/Space/SpaceDetail/handler.ts
+++ src/pages/Space/SpaceDetail/handler.ts
@@ -1,48 +1,4 @@
-import { useModalStore } from '@stores/useModalStore';
-import { useSpaceStart } from '@hooks/useSpace';
 import { useTabRun, useCodeSubmit } from '@hooks/useIDE';
-import { useTestCaseUpdate } from '@hooks/useTestCase';
-
-export function useTestCaseOpen(testCaseStatus, testCaseList, setTestCaseList, TestCaseSubmitHandler) {
-  const { open } = useModalStore();
-
-  const testCaseOpenHandler = () => {
-    open('testCase', {
-      status: testCaseStatus,
-      testCases: testCaseList,
-      setTestCaseList,
-      onSubmit: TestCaseSubmitHandler,
-    });
-  };
-
-  return { testCaseOpenHandler };
-}
-
-export function useTestCaseSubmit(codingSpaceId, testCaseList, setTestCaseList) {
-  const { testCaseUpdateMutate } = useTestCaseUpdate();
-
-  const TestCaseSubmitHandler = async () => {
-    const filteredTestCases = testCaseList
-      ?.filter((testCase) => testCase.type !== 'BASE')
-      .map(({ input, output }) => ({ input, output }));
-    const newTestCases = await testCaseUpdateMutate.mutateAsync({ codingSpaceId, testCase: filteredTestCases });
-    const BaseTestCase = testCaseList?.filter((testCase) => testCase.type === 'BASE');
-    const FetchTestCaseList = BaseTestCase.concat(newTestCases);
-    setTestCaseList(FetchTestCaseList);
-  };
-
-  return { TestCaseSubmitHandler };
-}
-
-export function useSpaceStatusHandler(codingSpaceId, studyId, status) {
-  const { spaceStartMutate } = useSpaceStart();
-  const spaceStartHandler = () => {
-    if (status === '대기') {
-      spaceStartMutate.mutate({ codingSpaceId, studyId });
-    }
-  };
-  return { spaceStartHandler };
-}
 
 export function useCodeRun(tabInfo, input, language) {
   const { ideRun } = useTabRun();
