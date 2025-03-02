f797a6e62475b839ec86056a38219827336669dd
Seio924 <seio9241@gmail.com>
2025-03-02 16:12:42 +0900

    COC-172-refactor(#97): FilterTab 리팩토링

--- src/pages/Study/StudyList/FilterTab/style.ts
+++ src/pages/Study/StudyList/FilterTab/style.ts
@@ -1,14 +1,21 @@
 import styled from '@emotion/styled';
 
-export const FilterTabContainer = styled.div`
+const FilterTabContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 8.5rem 0 4rem 0;
+
   position: relative;
 `;
 
-export const DropdownWrapper = styled.div`
+const DropdownWrapper = styled.div`
   display: flex;
   gap: 2rem;
 `;
+
+const S = {
+  FilterTabContainer,
+  DropdownWrapper,
+};
+
+export default S;
