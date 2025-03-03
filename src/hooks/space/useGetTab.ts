f1156b87a42de9eca93320bf8b317677ae5ce7de
Seio924 <seio9241@gmail.com>
2025-03-04 01:52:49 +0900

    COC-172-refactor(#97): 탭 조회, 모든 탭 조회 구분

--- src/hooks/space/useGetTab.ts
+++ src/hooks/space/useGetTab.ts
@@ -3,10 +3,10 @@ import { useQuery } from '@tanstack/react-query';
 import spaceApi from '@api/domain/space';
 import QUERY_KEYS from '@constants/queryKeys';
 
-export default function useGetAllTabs(codingSpaceId: string) {
+export default function useGetTab(codingSpaceId: string) {
   return useQuery({
-    queryKey: [QUERY_KEYS.SPACE_ALL_TAB, codingSpaceId],
-    queryFn: () => spaceApi.getAllTabs(codingSpaceId),
+    queryKey: [QUERY_KEYS.SPACE_TAB, codingSpaceId],
+    queryFn: () => spaceApi.getTab(codingSpaceId),
     enabled: !!codingSpaceId,
   });
 }
