3b355c6c9e1fe692726136eac44b0d449c54fcbc
Seio924 <seio9241@gmail.com>
2025-03-04 03:00:32 +0900

    COC-172-refactor(#97): 코드 제출, 실행 훅 리팩토링

---
+++ src/hooks/ide/useRunIDE.ts
@@ -0,0 +1,10 @@
+import ideApi from '@api/domain/ide';
+import { useMutation } from '@tanstack/react-query';
+
+export default function useRunIDE() {
+  const runIDEMutate = useMutation({
+    mutationFn: ideApi.run,
+  });
+
+  return { runIDEMutate };
+}
