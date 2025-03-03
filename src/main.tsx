10aead9667665b8a04eeab1f63527171f45d5cab
Seio924 <seio9241@gmail.com>
2025-03-03 22:52:44 +0900

    COC-172-refactor(#97): QueryClientProvider main으로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useToastStore } from '@stores/useToastStore';
 import ToastList from '@components/_common/molecules/ToastList';
 import { worker } from '@mocks/browser';
 import BaseModal from '@components/Modal/BaseModal';
@@ -17,13 +20,30 @@ if (import.meta.env.MODE === 'development') {
   });
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+      retry: 0,
+    },
+    mutations: {
+      onError: (error) => {
+        useToastStore.getState().error(error.message);
+      },
+    },
+  },
+});
+
 const root = createRoot(document.getElementById('root') as Element);
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <Global styles={globalStyles()} />
-    <ToastList />
-    <BaseModal />
-    <AppRouter />
-  </ThemeProvider>,
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles()} />
+      <ToastList />
+      <BaseModal />
+      <AppRouter />
+    </ThemeProvider>
+  </QueryClientProvider>,
 );
