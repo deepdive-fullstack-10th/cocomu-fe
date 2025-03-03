1b3dab42379dad5794190520c715eec26956ca7e
Seio924 <seio9241@gmail.com>
2025-03-03 22:51:25 +0900

    COC-172-refactor(#97): space mock handler 리팩토링

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,17 +1,6 @@
 import { authHandlers } from './auth';
 import { studyHandlers } from './study';
-import { testCaseHandlers } from './testCase';
-import { spaceDetailHandlers, spaceHandlers, spaceStartHandlers, tabDataHandlers } from './space';
+import { spaceHandlers } from './space';
 import { ideRunHandlers, ideSubmitHandler } from './ide';
 
-export const handlers = [
-  ...authHandlers,
-  ...spaceDetailHandlers,
-  ...studyHandlers,
-  ...spaceStartHandlers,
-  ...tabDataHandlers,
-  ...testCaseHandlers,
-  ...ideRunHandlers,
-  ...ideSubmitHandler,
-  ...spaceHandlers,
-];
+export const handlers = [...authHandlers, ...studyHandlers, ...spaceHandlers, ...ideRunHandlers, ...ideSubmitHandler];
