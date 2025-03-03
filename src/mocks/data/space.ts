ab0dc418ba262b1a709865cb384a70cce5ea7f18
Seio924 <seio9241@gmail.com>
2025-03-03 22:50:51 +0900

    COC-172-refactor(#97): testcase 수정 data 위치 이동

--- src/mocks/data/space.ts
+++ src/mocks/data/space.ts
@@ -135,3 +135,41 @@ export const createErrorResponse = {
   code: 4200,
   message: '유효성 검사 실패',
 };
+
+export const updateTestCaseResponse = {
+  code: 1200,
+  message: '테스트 케이스 수정됨',
+  result: {
+    testCases: [
+      {
+        id: 100,
+        input: '1,2,3,4,5',
+        output: '8',
+        type: 'CUSTOM',
+      },
+      {
+        id: 101,
+        input: '1,2,3,4,5',
+        output: '8',
+        type: 'CUSTOM',
+      },
+      {
+        id: 102,
+        input: '1,2,3,4,5',
+        output: '8',
+        type: 'CUSTOM',
+      },
+      {
+        id: 103,
+        input: '1,2,3,4,5',
+        output: '8',
+        type: 'CUSTOM',
+      },
+    ],
+  },
+};
+
+export const updateTestCaseErrorResponse = {
+  code: 4200,
+  message: '테스트 케이스 추가에 실패했습니다.',
+};
