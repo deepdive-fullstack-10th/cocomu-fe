200e48b377cd809e10e5b5bb6d4b09cf944a7fe5
Seio924 <seio9241@gmail.com>
2025-03-04 01:52:49 +0900

    COC-172-refactor(#97): 탭 조회, 모든 탭 조회 구분

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -3,6 +3,7 @@ const QUERY_KEYS = {
   STUDY_LIST: 'study_list',
   SPACE_DETAIL: 'space_detail',
   SPACE_TAB: 'space_tab',
+  SPACE_ALL_TAB: 'space_all_tab',
 } as const;
 
 export default QUERY_KEYS;
