71c8e4d31081446bf0bbc50b3455b96b5284b563
Re-Note <dldydtn207@gmail.com>
2025-03-04 11:29:08 +0900

    COC-93-feat(#91): StudyInfo 레이아웃 구현, api 적용, 라우팅 적용, 기타 컴포넌트 스타일 수정

--- src/pages/Study/StudyInfo/index.tsx
+++ src/pages/Study/StudyInfo/index.tsx
@@ -1,3 +1,105 @@
+import { useState } from 'react';
+import { useOutletContext } from 'react-router-dom'; // Outlet을 통해 studyId 가져오기
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import UserProfile from '@components/_common/molecules/UserProfile';
+import Tag from '@components/_common/atoms/Tag';
+import Button from '@components/_common/atoms/Button';
+import DropdownItem from '@components/_common/atoms/DropdownItem';
+import useGetStudyInfo from '@hooks/study/useGetStudyInfo';
+import { formatDate } from '@utils/formatDate';
+import S from './style';
+
+interface StudyContext {
+  studyId: string;
+}
+
 export default function StudyInfo() {
-  return <div>StudyInfo</div>;
+  const { studyId } = useOutletContext<StudyContext>();
+  const { data } = useGetStudyInfo(studyId);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  /* TODO: 임시로 현재 로그인 한 user id 지정, 추후에 수정하기 */
+  const isLogined = 1;
+
+  const {
+    name = '스터디 이름 불러오는 중...',
+    status = 'PRIVATE',
+    createdAt = '',
+    description = '설명 없음',
+    languages = [],
+    totalUserCount = 0,
+    leader = {
+      id: 0,
+      nickName: '알 수 없음',
+      profileImageUrl: 'https://cdn.cocomu.co.kr/images/default/profile.png',
+    },
+  } = data || {};
+
+  const isLeader = isLogined === leader.id;
+
+  return (
+    <S.Container>
+      <S.Header>
+        <S.StudyTitle>{name}</S.StudyTitle>
+        {isLeader ? (
+          <S.IconContainer>
+            <BsThreeDotsVertical onClick={() => setDropdownOpen(!dropdownOpen)} />
+            {dropdownOpen && (
+              <S.Dropdown>
+                <DropdownItem
+                  item='스터디 수정하기'
+                  onClick={() => console.log('스터디 수정 클릭')}
+                />
+                <DropdownItem
+                  item='스터디 삭제하기'
+                  onClick={() => console.log('스터디 삭제 클릭')}
+                />
+              </S.Dropdown>
+            )}
+          </S.IconContainer>
+        ) : (
+          <Button
+            color='triadic'
+            size='md'
+            onClick={() => console.log('스터디 나가기 클릭')}
+          >
+            스터디 나가기
+          </Button>
+        )}
+      </S.Header>
+      <S.BodyContainer>
+        <UserProfile
+          user={leader}
+          size='md'
+        />
+        <S.TagContainer>
+          <S.TagText>
+            공개 여부
+            <Tag color='primary'>{status === 'PUBLIC' ? '공개' : '비공개'}</Tag>
+          </S.TagText>
+          <S.TagText>
+            모집 인원
+            <Tag color='secondary'>
+              {totalUserCount}
+              {' 명'}
+            </Tag>
+          </S.TagText>
+          <S.TagText>
+            주력 언어
+            <Tag color='analogous'>{languages.join(', ') || '정보 없음'}</Tag>
+          </S.TagText>
+          <S.TagText>
+            시작 날짜
+            <Tag color='triadic'>{formatDate(createdAt) || '정보 없음'}</Tag>
+          </S.TagText>
+          <S.TagText>
+            사용 플랫폼
+            <Tag color='gray'>백준</Tag>
+          </S.TagText>
+        </S.TagContainer>
+        <S.ProjectIntro>프로젝트 소개</S.ProjectIntro>
+        <S.Description>{description}</S.Description>
+      </S.BodyContainer>
+    </S.Container>
+  );
 }
