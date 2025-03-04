ce642342062366f1726d23fd6d0581dfba0cbe9b
Seio924 <seio9241@gmail.com>
2025-03-04 15:30:03 +0900

    COC-47-feat(#100): 로그인 성공 시 메인페이지로 이동

--- src/hooks/auth/useLogIn.ts
+++ src/hooks/auth/useLogIn.ts
@@ -1,14 +1,20 @@
 import { axiosInstance } from '@api/axiosInstance';
 import authApi from '@api/domain/auth';
 import { ACCESS_TOKEN_KEY } from '@constants/api';
+import { ROUTES } from '@constants/path';
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 
 export default function useLogIn() {
+  const navigate = useNavigate();
+
   const logInMutate = useMutation({
     mutationFn: authApi.login,
     onSuccess: ({ accessToken }) => {
       localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
       axiosInstance.defaults.headers.Authorization = `Bearer ${accessToken}`;
+
+      navigate(ROUTES.ROOT());
     },
   });
 
