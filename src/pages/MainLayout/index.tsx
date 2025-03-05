db60d33a68cdf06b1a678e1934bdf7bac0c812e8
Seio924 <seio9241@gmail.com>
2025-03-05 12:27:52 +0900

    COC-47-feat(#100): 로그인 여부에 따른 유저 정보 조회

--- src/pages/MainLayout/index.tsx
+++ src/pages/MainLayout/index.tsx
@@ -1,14 +1,27 @@
-import NavBar from '@components/NavBar';
-import { NAVBAR_DROPDOWN_LABELS } from '@constants/constants';
 import { Outlet } from 'react-router-dom';
+
+import useGetUserInfo from '@hooks/user/useGetUserInfo';
+import { useUserStore } from '@stores/useUserStore';
+
+import NavBar from '@components/NavBar';
+import Loading from '@pages/Loading';
+
+import { NAVBAR_DROPDOWN_LABELS } from '@constants/constants';
+
 import S from './style';
 
 export default function MainLayout() {
+  const isLoggedIn = useUserStore((state) => state.isLoggedIn);
+  const { data, isLoading } = useGetUserInfo({ enabled: isLoggedIn });
+
+  if (isLoading) return <Loading />;
+
   return (
     <S.MainContainer>
       <NavBar
         items={NAVBAR_DROPDOWN_LABELS}
-        isLogined={false}
+        isLoggedIn={isLoggedIn}
+        user={data}
       />
       <S.Container>
         <Outlet />
