b98e6504b6de59486c9825b5da75d0f55d0fd1ee
Seio924 <seio9241@gmail.com>
2025-03-05 12:28:13 +0900

    COC-47-feat(#100): 로그인 성공 시 로그인 여부 확인 추가

--- src/hooks/auth/useLogIn.ts
+++ src/hooks/auth/useLogIn.ts
@@ -2,10 +2,12 @@ import { axiosInstance } from '@api/axiosInstance';
 import authApi from '@api/domain/auth';
 import { ACCESS_TOKEN_KEY } from '@constants/api';
 import { ROUTES } from '@constants/path';
+import { useUserStore } from '@stores/useUserStore';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 export default function useLogIn() {
+  const checkAuth = useUserStore((state) => state.checkAuth);
   const navigate = useNavigate();
 
   const logInMutate = useMutation({
@@ -14,6 +16,7 @@ export default function useLogIn() {
       localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
       axiosInstance.defaults.headers.Authorization = `Bearer ${accessToken}`;
 
+      checkAuth();
       navigate(ROUTES.ROOT());
     },
   });
