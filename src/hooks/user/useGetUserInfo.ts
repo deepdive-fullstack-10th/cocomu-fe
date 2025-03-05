7901a39ef0fd8f901ab8ac68134721a2539bf298
Seio924 <seio9241@gmail.com>
2025-03-05 12:27:04 +0900

    COC-47-feat(#100): 로그인 여부에 따른 조건적 호출 추가

--- src/hooks/user/useGetUserInfo.ts
+++ src/hooks/user/useGetUserInfo.ts
@@ -3,9 +3,10 @@ import { useQuery } from '@tanstack/react-query';
 import QUERY_KEYS from '@constants/queryKeys';
 import userApi from '@api/domain/user';
 
-export default function useGetUserInfo() {
+export default function useGetUserInfo({ enabled }: { enabled: boolean }) {
   return useQuery({
     queryKey: [QUERY_KEYS.USER_DETAIL],
-    queryFn: async () => userApi.get,
+    queryFn: async () => userApi.get(),
+    enabled,
   });
 }
