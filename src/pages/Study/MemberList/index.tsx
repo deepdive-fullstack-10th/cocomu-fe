d7b90c39c5b0837df4206b0578effdf4fe4a2964
KimBeomJo <dongjun1315@naver.com>
2025-03-06 11:05:38 +0900

    COC-110-feat(#102): 멤버 리스트 조회 기초 구현

--- src/pages/Study/MemberList/index.tsx
+++ src/pages/Study/MemberList/index.tsx
@@ -1,3 +1,25 @@
+import UserCard from '@components/UserCard';
+import S from './style';
+
 export default function MemberList() {
-  return <div>MemberList</div>;
+  return (
+    <S.UserCardContainer>
+      <UserCard
+        successCount={32}
+        failedCount={2}
+        joinedDate='2025.01.28'
+        id={1}
+        nickName='김서현'
+        profileImageUrl='https://cdn.cocomu.co.kr/images/default/profile.png'
+      />
+      <UserCard
+        successCount={32}
+        failedCount={2}
+        joinedDate='2025.01.28'
+        id={2}
+        nickName='김서현'
+        profileImageUrl='https://cdn.cocomu.co.kr/images/default/profile.png'
+      />
+    </S.UserCardContainer>
+  );
 }
---
+++ src/pages/Study/MemberList/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const UserCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const S = {
+  UserCardContainer,
+};
+
+export default S;
