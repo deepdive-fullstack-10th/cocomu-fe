b063cb65cbb811a7b7d3c40d7ee9a40a00f2fb58
KimBeomJo <dongjun1315@naver.com>
2025-03-07 12:41:30 +0900

    COC-110-fix(#102): 참여일 렌더링 관련 수정

--- src/pages/Study/MemberList/index.tsx
+++ src/pages/Study/MemberList/index.tsx
@@ -1,25 +1,48 @@
 import UserCard from '@components/UserCard';
+import useGetMemberList from '@hooks/study/useGetMemberList';
+import { useParams } from 'react-router-dom';
+import { UserDetailData } from '@customTypes/user';
+import LoadingSpinner from '@components/_common/atoms/LoadingSpinner';
+import useScroll from '@hooks/utils/useScroll';
 import S from './style';
 
 export default function MemberList() {
+  const { studyId } = useParams<{ studyId: string }>();
+  const { userList, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } = useGetMemberList(studyId);
+  const { observerRef, isFetching } = useScroll({
+    nextPage: hasNextPage,
+    fetchingNextPage: isFetchingNextPage,
+    fetchNext: fetchNextPage,
+    delayTime: 1000,
+  });
+
   return (
     <S.UserCardContainer>
-      <UserCard
-        successCount={32}
-        failedCount={2}
-        joinedDate='2025.01.28'
-        id={1}
-        nickName='김서현'
-        profileImageUrl='https://cdn.cocomu.co.kr/images/default/profile.png'
-      />
-      <UserCard
-        successCount={32}
-        failedCount={2}
-        joinedDate='2025.01.28'
-        id={2}
-        nickName='김서현'
-        profileImageUrl='https://cdn.cocomu.co.kr/images/default/profile.png'
-      />
+      {userList.map((user: UserDetailData) => (
+        <UserCard
+          key={user.id}
+          id={user.id}
+          successCount={user.successCount}
+          failedCount={user.failedCount}
+          joinedDate={user.joinedDate}
+          nickName={user.nickName}
+          profileImageUrl={user.profileImageUrl}
+        />
+      ))}
+      {(hasNextPage || isLoading || isFetching) && (
+        <div
+          ref={observerRef}
+          style={{
+            height: '0.2rem',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingBottom: '4rem',
+          }}
+        >
+          <LoadingSpinner />
+        </div>
+      )}
     </S.UserCardContainer>
   );
 }
