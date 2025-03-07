bc678f2735d398a754453b3aff334a20444414d0
Seio924 <seio9241@gmail.com>
2025-03-08 03:21:13 +0900

    COC-178-refactor(#111): 스터디 정보 컴포넌트 분리

---
+++ src/components/Study/StudyDescription/DescriptionHeader/index.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+
+import UserProfile from '@components/_common/molecules/UserProfile';
+import Icon from '@components/_common/atoms/Icon';
+import Button from '@components/_common/atoms/Button';
+import DropdownItem from '@components/_common/atoms/DropdownItem';
+
+import { STUDY_EDIT_DROPDOWN_LABELS } from '@constants/common';
+import { ROUTES } from '@constants/path';
+import { UserData } from '@customTypes/user';
+
+import S from './style';
+
+interface DescriptionHeaderProps {
+  isLeader: boolean;
+  isStudy: boolean;
+  leader: UserData;
+  studyId: number;
+}
+
+export default function DescriptionHeader({ isLeader, isStudy, leader, studyId }: DescriptionHeaderProps) {
+  const navigate = useNavigate();
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => setDropdownOpen((prev) => !prev);
+
+  const handleItemSelect = (selectedItem: string) => {
+    if (selectedItem === STUDY_EDIT_DROPDOWN_LABELS[0].label) {
+      navigate(ROUTES.STUDY.EDIT({ studyId }));
+    } else if (selectedItem === STUDY_EDIT_DROPDOWN_LABELS[1].label) {
+      // TODO: 스터디 삭제 API 호출
+    }
+    setDropdownOpen(false);
+  };
+
+  const handleLeave = () => {};
+
+  return (
+    <S.Header>
+      <UserProfile
+        user={leader}
+        size='md'
+      />
+
+      {isLeader ? (
+        <S.IconWrapper>
+          <Icon
+            size='md'
+            color='950'
+            onClick={handleDropdownToggle}
+          >
+            <BsThreeDotsVertical />
+          </Icon>
+          {isDropdownOpen && (
+            <S.DropdownList>
+              {STUDY_EDIT_DROPDOWN_LABELS.map(({ label, color }) => (
+                <DropdownItem
+                  key={label}
+                  item={label}
+                  size='lg'
+                  color={color}
+                  onClick={() => handleItemSelect(label)}
+                />
+              ))}
+            </S.DropdownList>
+          )}
+        </S.IconWrapper>
+      ) : (
+        isStudy && (
+          <Button
+            color='triadic'
+            size='md'
+            onClick={handleLeave}
+          >
+            스터디 나가기
+          </Button>
+        )
+      )}
+    </S.Header>
+  );
+}
---
+++ src/components/Study/StudyDescription/index.tsx
@@ -0,0 +1,72 @@
+import Tag from '@components/_common/atoms/Tag';
+import { formatDate } from '@utils/formatDate';
+import TagList from '@components/_common/molecules/TagList';
+import TextEditor from '@components/_common/atoms/TextEditor';
+import { StudyData } from '@customTypes/study';
+import { useUserStore } from '@stores/useUserStore';
+
+import S from './style';
+import DescriptionHeader from './DescriptionHeader';
+
+interface StudyDescriptionProps extends Omit<StudyData, 'currentUserCount' | 'joinable'> {
+  isStudy?: boolean;
+}
+
+export default function StudyDescription({
+  id,
+  name,
+  status,
+  languages,
+  workbooks,
+  description,
+  totalUserCount,
+  createdAt,
+  leader,
+  isStudy,
+}: StudyDescriptionProps) {
+  const userId = useUserStore((state) => state.userId);
+
+  return (
+    <S.Container>
+      <DescriptionHeader
+        isLeader={leader.id === userId}
+        isStudy={isStudy}
+        leader={leader}
+        studyId={id}
+      />
+
+      <S.Body>
+        <S.StudyName>{name}</S.StudyName>
+
+        <S.TagContainer>
+          <S.Label>공개 여부</S.Label>
+          <Tag color='primary'>{status === 'PUBLIC' ? '공개' : '비공개'}</Tag>
+
+          <S.Label>모집 인원</S.Label>
+          <Tag color='secondary'>{`${totalUserCount} 명`}</Tag>
+
+          <S.Label>시작 날짜</S.Label>
+          <Tag color='triadic'>{formatDate(createdAt) || '정보 없음'}</Tag>
+
+          <S.Label>주력 언어</S.Label>
+          <TagList
+            items={languages}
+            color='analogous'
+          />
+
+          <S.Label>사용 플랫폼</S.Label>
+          <TagList
+            items={workbooks}
+            color='gray'
+          />
+        </S.TagContainer>
+
+        <S.StudyIntro>스터디 소개</S.StudyIntro>
+        <TextEditor
+          value={description}
+          readOnly
+        />
+      </S.Body>
+    </S.Container>
+  );
+}
