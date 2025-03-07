b3096c851959fa4a0fc299863c6662c3d60af0b4
Seio924 <seio9241@gmail.com>
2025-03-07 22:18:46 +0900

    COC-178-feat(#111): 필터 옵션 조회 훅 구현

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -2,6 +2,7 @@ const QUERY_KEYS = {
   USER_DETAIL: 'user_detail',
   STUDY_DETAIL: 'study_detail',
   STUDY_LIST: 'study_list',
+  STUDY_FILTER: 'study_filter',
   SPACE_DETAIL: 'space_detail',
   SPACE_TAB: 'space_tab',
   SPACE_ALL_TAB: 'space_all_tab',
---
+++ src/hooks/study/useGetFilterOptions.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+
+import studyApi from '@api/domain/study';
+import QUERY_KEYS from '@constants/queryKeys';
+
+export default function useGetFilterOptions() {
+  return useQuery({
+    queryKey: [QUERY_KEYS.STUDY_FILTER],
+    queryFn: () => studyApi.getFilterOptions(),
+  });
+}
