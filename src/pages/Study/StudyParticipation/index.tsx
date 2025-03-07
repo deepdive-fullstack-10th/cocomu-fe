17d4bad59c3e825922fb3fbff0231546393595a4
Seio924 <seio9241@gmail.com>
2025-03-08 03:22:05 +0900

    COC-178-refactor(#111): 스터디 참가 페이지 리팩토링

--- src/pages/Study/StudyParticipation/index.tsx
+++ src/pages/Study/StudyParticipation/index.tsx
@@ -1,15 +1,41 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { BsArrowLeft } from 'react-icons/bs';
 
 import { ROUTES } from '@constants/path';
 
+import { useModalStore } from '@stores/useModalStore';
+
+import useGetStudyInfo from '@hooks/study/useGetStudyInfo';
+
 import IconButton from '@components/_common/atoms/IconButton';
+import StudyDescription from '@components/Study/StudyDescription';
+import Button from '@components/_common/atoms/Button';
+
+import Loading from '@pages/Loading';
 
 import S from './style';
 
 export default function StudyParticipation() {
   const navigate = useNavigate();
+  const { open } = useModalStore();
+  const { studyId } = useParams<{ studyId: string }>();
+  const { data, isLoading } = useGetStudyInfo(studyId);
+
+  const handleJoinClick = () => {
+    if (data.status === 'PUBLIC') {
+      open('confirm', {
+        studyId,
+        name: data.name,
+      });
+    } else if (data.status === 'PRIVATE') {
+      open('passwordInput', {
+        studyId,
+      });
+    }
+  };
+
+  if (isLoading) return <Loading />;
 
   return (
     <S.Container>
@@ -22,7 +48,28 @@ export default function StudyParticipation() {
         </IconButton>
       </S.ButtonWrapper>
 
-      <Outlet />
+      <StudyDescription
+        id={data.id}
+        name={data.name}
+        status={data.status}
+        languages={data.languages}
+        workbooks={data.workbooks}
+        description={data.description}
+        totalUserCount={data.totalUserCount}
+        createdAt={data.createdAt}
+        leader={data.leader}
+      />
+
+      <S.Footer>
+        <Button
+          size='lg'
+          color='primary'
+          shape='default'
+          onClick={handleJoinClick}
+        >
+          참여하기
+        </Button>
+      </S.Footer>
     </S.Container>
   );
 }
