e1f67ae572eb8a07f7b1e88636ef5f35d9496433
Seio924 <seio9241@gmail.com>
2025-03-07 22:19:04 +0900

    COC-178-feat(#111): 필터 옵션 조회 mock 데이터 설정

--- src/mocks/data/study.ts
+++ src/mocks/data/study.ts
@@ -88,6 +88,27 @@ export const getStudyInfoErrorResponse = {
   message: '스터디 정보 조회에 실패했습니다.',
 };
 
+export const getFilterOptionsResponse = {
+  code: 3000,
+  message: '스터디 필터 옵션 조회에 성공했습니다.',
+  result: {
+    workbooks: [
+      {
+        id: 1,
+        name: 'BOJ',
+        imageUrl: 'https://cdn.cocomu.co.kr/images/workbooks/boj.png',
+      },
+    ],
+    languages: [
+      {
+        id: 1,
+        name: 'JAVA',
+        imageUrl: 'https://cdn.cocomu.co.kr/images/languages/java.png',
+      },
+    ],
+  },
+};
+
 export const createPublicResponse = {
   code: 1200,
   message: '공개 스터디 생성에 성공했습니다.',
