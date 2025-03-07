fddddbd78eaa11c9a0f0af8edf286fa01a431d19
KimBeomJo <dongjun1315@naver.com>
2025-03-07 09:42:57 +0900

    COC-110-feat(#102): useScroll 커스텀 훅 구현 및 커스텀 훅 스페이스 리스트에 적용

---
+++ src/hooks/utils/useScroll.ts
@@ -0,0 +1,53 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { FetchNextPageOptions, UseInfiniteQueryResult } from '@tanstack/react-query';
+
+interface useScrollProps {
+  nextPage?: boolean;
+  fetchingNextPage?: boolean;
+  fetchNext?: (options?: FetchNextPageOptions) => Promise<UseInfiniteQueryResult>;
+  thresholdRate?: number;
+}
+
+export default function useScroll({ nextPage, fetchingNextPage, fetchNext, thresholdRate = 0.1 }: useScrollProps) {
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const observerRef = useRef<HTMLDivElement>(null);
+
+  const nextList = useCallback(async () => {
+    if (isFetching || !nextPage || fetchingNextPage) return;
+
+    try {
+      setIsFetching(true);
+      await fetchNext();
+    } catch (error) {
+      console.error('무한 스크롤 에러:', error);
+    } finally {
+      setTimeout(() => {
+        setIsFetching(false);
+      }, 200);
+    }
+  }, [nextPage, isFetching, fetchingNextPage, fetchNext]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && nextPage && !fetchingNextPage) {
+          nextList().catch((e) => {
+            console.error('스크롤 에러', e);
+          });
+        }
+      },
+      { threshold: thresholdRate },
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, [nextPage, fetchingNextPage, nextList]);
+
+  return {
+    observerRef,
+    isFetching,
+  };
+}
