45d5c4334d537dbd511060d9250b617f8b355241
Seio924 <seio9241@gmail.com>
2025-03-08 14:38:06 +0900

    COC-178-feat(#111): 스페이스 참여 mock data 생성

---
+++ src/mocks/data/space/joinSpaceData.ts
@@ -0,0 +1,12 @@
+export const joinSpaceResponse = {
+  code: 4000,
+  message: '코딩 스페이스 참여에 성공했습니다.',
+  result: {
+    codingSpaceId: 33,
+  },
+};
+
+export const joinSpaceErrorResponse = {
+  code: 4004,
+  message: '코딩 스페이스 참여에 실패했습니다.',
+};
