c93eea246c39794936a8806a6e67aa9d441f86e9
Seio924 <seio9241@gmail.com>
2025-03-08 20:06:51 +0900

    COC-178-feat(#111): 스페이스 스터디 정보 조회 mock handler 구현

--- src/mocks/handlers/space.ts
+++ src/mocks/handlers/space.ts
@@ -2,11 +2,6 @@ import { http, HttpResponse } from 'msw';
 import {
   createErrorResponse,
   createResponse,
-  getSpaceErrorResponse,
-  getSpaceResponse,
-  getTabErrorResponse,
-  getTabResponse,
-  getAllTabsResponse,
   spaceStartErrorResponse,
   spaceStartResponse,
   updateTestCaseErrorResponse,
@@ -19,96 +14,24 @@ import {
 import { BASE_URL, END_POINTS_V1, HTTP_STATUS_CODE } from '@constants/api';
 import { CreateSpaceData, TestCaseIO } from '@customTypes/space';
 import { joinSpaceErrorResponse, joinSpaceResponse } from '@mocks/data/space/joinSpaceData';
+import { getSpaceStudyInfoErrorResponse, getSpaceStudyInfoResponse } from '@mocks/data/space/getSpaceStudyInfoData';
 
 export const spaceHandlers = [
-  http.post(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.CREATE}`, async ({ request }) => {
-    const body = (await request.json()) as CreateSpaceData;
+  http.get(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.INFO}`, async ({ request }) => {
+    const url = new URL(request.url);
+    const studyId = url.searchParams.get('studyId');
 
-    if (!body.studyId || !body.codingSpace || !body.testCases) {
-      return new HttpResponse(JSON.stringify(createErrorResponse), {
+    if (!studyId) {
+      return new HttpResponse(JSON.stringify(getSpaceStudyInfoErrorResponse), {
         status: HTTP_STATUS_CODE.BAD_REQUEST,
       });
     }
 
-    return new HttpResponse(JSON.stringify(createResponse), {
+    return new HttpResponse(JSON.stringify(getSpaceStudyInfoResponse), {
       status: HTTP_STATUS_CODE.SUCCESS,
     });
   }),
 
-  http.get(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.TAB(':codingSpaceId')}`, async ({ params }) => {
-    const { codingSpaceId } = params;
-
-    if (!codingSpaceId) {
-      return new HttpResponse(JSON.stringify(getTabErrorResponse), {
-        status: HTTP_STATUS_CODE.BAD_REQUEST,
-      });
-    }
-
-    return new HttpResponse(JSON.stringify(getTabResponse), {
-      status: HTTP_STATUS_CODE.SUCCESS,
-    });
-  }),
-
-  http.get(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.ALL_TABS(':codingSpaceId')}`, async ({ params }) => {
-    const { codingSpaceId } = params;
-
-    if (!codingSpaceId) {
-      return new HttpResponse(JSON.stringify(getTabErrorResponse), {
-        status: HTTP_STATUS_CODE.BAD_REQUEST,
-      });
-    }
-
-    return new HttpResponse(JSON.stringify(getAllTabsResponse), {
-      status: HTTP_STATUS_CODE.SUCCESS,
-    });
-  }),
-
-  http.post(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.START(':codingSpaceId')}`, async ({ params, request }) => {
-    const body = await request.json();
-    const { codingSpaceId } = params;
-
-    if (!body || !codingSpaceId) {
-      return new HttpResponse(JSON.stringify(spaceStartErrorResponse), {
-        status: HTTP_STATUS_CODE.BAD_REQUEST,
-      });
-    }
-
-    return new HttpResponse(JSON.stringify(spaceStartResponse), {
-      status: HTTP_STATUS_CODE.SUCCESS,
-    });
-  }),
-
-  http.get(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.PAGE(':codingSpaceId')}`, async ({ params }) => {
-    const { codingSpaceId } = params;
-
-    if (!codingSpaceId) {
-      return new HttpResponse(JSON.stringify(getSpaceErrorResponse), {
-        status: HTTP_STATUS_CODE.BAD_REQUEST,
-      });
-    }
-
-    return new HttpResponse(JSON.stringify(getSpaceResponse), {
-      status: HTTP_STATUS_CODE.SUCCESS,
-    });
-  }),
-
-  http.post(
-    `${BASE_URL}${END_POINTS_V1.CODING_SPACE.TEST_CASE_UPDATE(':codingSpaceId')}`,
-    async ({ params, request }) => {
-      const { codingSpaceId } = params;
-      const body = (await request.json()) as TestCaseIO[];
-
-      if (!codingSpaceId || !body) {
-        return new HttpResponse(JSON.stringify(updateTestCaseErrorResponse), {
-          status: HTTP_STATUS_CODE.BAD_REQUEST,
-        });
-      }
-
-      return new HttpResponse(JSON.stringify(updateTestCaseResponse), {
-        status: HTTP_STATUS_CODE.SUCCESS,
-      });
-    },
-  ),
   http.get(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.LIST(':studyId')}`, async ({ request, params }) => {
     const { studyId } = params;
     if (!studyId || Number.isNaN(Number(studyId))) {
@@ -141,17 +64,16 @@ export const spaceHandlers = [
     });
   }),
 
-  http.post(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.COMPLETE(':codingSpaceId')}`, async ({ params, request }) => {
-    const body = await request.json();
-    const { codingSpaceId } = params;
+  http.post(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.CREATE}`, async ({ request }) => {
+    const body = (await request.json()) as CreateSpaceData;
 
-    if (!body || !codingSpaceId) {
-      return new HttpResponse(JSON.stringify(completeSpaceErrorResponse), {
+    if (!body.studyId || !body.codingSpace || !body.testCases) {
+      return new HttpResponse(JSON.stringify(createErrorResponse), {
         status: HTTP_STATUS_CODE.BAD_REQUEST,
       });
     }
 
-    return new HttpResponse(JSON.stringify(completeSpaceResponse), {
+    return new HttpResponse(JSON.stringify(createResponse), {
       status: HTTP_STATUS_CODE.SUCCESS,
     });
   }),
@@ -166,6 +88,54 @@ export const spaceHandlers = [
     }
 
     return new HttpResponse(JSON.stringify(joinSpaceResponse), {
+      status: HTTP_STATUS_CODE.SUCCESS,
+    });
+  }),
+
+  http.post(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.START(':codingSpaceId')}`, async ({ params, request }) => {
+    const body = await request.json();
+    const { codingSpaceId } = params;
+
+    if (!body || !codingSpaceId) {
+      return new HttpResponse(JSON.stringify(spaceStartErrorResponse), {
+        status: HTTP_STATUS_CODE.BAD_REQUEST,
+      });
+    }
+
+    return new HttpResponse(JSON.stringify(spaceStartResponse), {
+      status: HTTP_STATUS_CODE.SUCCESS,
+    });
+  }),
+
+  http.post(
+    `${BASE_URL}${END_POINTS_V1.CODING_SPACE.TEST_CASE_UPDATE(':codingSpaceId')}`,
+    async ({ params, request }) => {
+      const { codingSpaceId } = params;
+      const body = (await request.json()) as TestCaseIO[];
+
+      if (!codingSpaceId || !body) {
+        return new HttpResponse(JSON.stringify(updateTestCaseErrorResponse), {
+          status: HTTP_STATUS_CODE.BAD_REQUEST,
+        });
+      }
+
+      return new HttpResponse(JSON.stringify(updateTestCaseResponse), {
+        status: HTTP_STATUS_CODE.SUCCESS,
+      });
+    },
+  ),
+
+  http.post(`${BASE_URL}${END_POINTS_V1.CODING_SPACE.COMPLETE(':codingSpaceId')}`, async ({ params, request }) => {
+    const body = await request.json();
+    const { codingSpaceId } = params;
+
+    if (!body || !codingSpaceId) {
+      return new HttpResponse(JSON.stringify(completeSpaceErrorResponse), {
+        status: HTTP_STATUS_CODE.BAD_REQUEST,
+      });
+    }
+
+    return new HttpResponse(JSON.stringify(completeSpaceResponse), {
       status: HTTP_STATUS_CODE.SUCCESS,
     });
   }),
