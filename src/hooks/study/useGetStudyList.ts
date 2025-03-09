b685f5be3ea91c512ec2733a4eeb8f0c597dc2e0
Seio924 <seio9241@gmail.com>
2025-03-09 18:09:02 +0900

    COC-178-feat(#111): 스터디 목록 조회 staleTime 변경

--- src/hooks/study/useGetStudyList.ts
+++ src/hooks/study/useGetStudyList.ts
@@ -8,6 +8,6 @@ export default function useGetStudyList(params: GetListData) {
   return useQuery({
     queryKey: [QUERY_KEYS.STUDY_LIST, params],
     queryFn: () => studyApi.getList(params),
-    staleTime: 1000 * 60 * 2,
+    staleTime: 1000 * 60 * 1,
   });
 }
