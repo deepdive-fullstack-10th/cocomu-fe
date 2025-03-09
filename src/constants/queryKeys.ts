c8299faf3109d857a2ecf4e5267170da6c28b498
Seio924 <seio9241@gmail.com>
2025-03-09 10:40:20 +0900

    COC-178-feat(#111):쿼리키 변경 및 스터디 api 정리

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,11 +1,9 @@
 const QUERY_KEYS = {
   USER_DETAIL: 'user_detail',
+  STUDY_DETAIL: 'study_detail',
   STUDY_INFO: 'study_info',
   STUDY_LIST: 'study_list',
   STUDY_FILTER: 'study_filter',
-  SPACE_STUDY_INFO: 'space_study_info',
-  SPACE_TAB: 'space_tab',
-  SPACE_ALL_TAB: 'space_all_tab',
   STUDY_PARTICIPATION: 'study_participation',
 } as const;
 
--- src/hooks/study/useGetStudyList.ts
+++ src/hooks/study/useGetStudyList.ts
@@ -2,11 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 
 import studyApi from '@api/domain/study';
 import QUERY_KEYS from '@constants/queryKeys';
-import { GetListParams } from '@customTypes/study';
+import { GetListData } from '@customTypes/study';
 
-export default function useGetStudyList(params: GetListParams) {
+export default function useGetStudyList(params: GetListData) {
   return useQuery({
     queryKey: [QUERY_KEYS.STUDY_LIST, params],
-    queryFn: async () => studyApi.getList(params),
+    queryFn: () => studyApi.getList(params),
   });
 }
