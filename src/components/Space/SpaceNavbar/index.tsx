5be8e31df1c502e8e0e4d68c8dff778912edc9a6
Seio924 <seio9241@gmail.com>
2025-03-10 13:21:20 +0900

    COC-183-refactor(#119): SpaceNavbar 리팩토링

--- src/components/Space/SpaceNavbar/index.tsx
+++ src/components/Space/SpaceNavbar/index.tsx
@@ -1,79 +1,40 @@
+import { ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsArrowLeft } from 'react-icons/bs';
 
 import { ROUTES } from '@constants/path';
-import { STATUS_ACTIONS, STATUS } from '@constants/space';
 
-import useStartSpace from '@hooks/space/useStartSpace';
-import useCompleteSpace from '@hooks/space/useCompleteSpace';
 import IconButton from '@components/_common/atoms/IconButton';
-import Button from '@components/_common/atoms/Button';
 import Timer from '@components/_common/atoms/Timer';
-import { Tab } from '@customTypes/space';
+
 import S from './style';
 
 interface SpaceNavbarProps {
-  codingSpaceId: string;
-  studyId: string;
-  status: keyof typeof STATUS_ACTIONS;
-  isLeader: boolean;
+  studyId: number;
   name: string;
   timer?: number;
-  tabData?: Tab[];
+  children: ReactNode;
 }
 
-export default function SpaceNavbar({
-  codingSpaceId,
-  studyId,
-  status,
-  isLeader,
-  name,
-  timer,
-  tabData,
-}: SpaceNavbarProps) {
+export default function SpaceNavbar({ studyId, name, timer, children }: SpaceNavbarProps) {
   const navigate = useNavigate();
-  const { startSpaceMutate } = useStartSpace();
-  const { completeSpaceMutate } = useCompleteSpace();
-  const actionLabel = STATUS_ACTIONS[status]?.label;
-
-  const handleStart = () => {
-    if (status === STATUS.WAITING) {
-      startSpaceMutate.mutate({ codingSpaceId, studyId });
-    }
-    if (status === STATUS.FEEDBACK) {
-      completeSpaceMutate.mutate({ codingSpaceId, tabData });
-    }
-    if (status === STATUS.FINISHED) {
-      navigate(ROUTES.STUDY.DETAIL({ studyId }));
-    }
-  };
 
   return (
     <S.Container>
       <S.LeftSection>
-        {status !== STATUS.FINISHED && (
-          <IconButton
-            content='돌아가기'
-            align='center'
-            color='none'
-            onClick={() => navigate(ROUTES.STUDY.DETAIL({ studyId }))}
-          >
-            <BsArrowLeft />
-          </IconButton>
-        )}
+        <IconButton
+          content='돌아가기'
+          align='center'
+          color='none'
+          onClick={() => navigate(ROUTES.STUDY.DETAIL({ studyId }))}
+        >
+          <BsArrowLeft />
+        </IconButton>
         <S.Name>{name}</S.Name>
       </S.LeftSection>
       <S.RightSection>
         {timer && <Timer timer={timer} />}
-        {actionLabel && (status !== STATUS.WAITING || isLeader) && (
-          <Button
-            size='md'
-            color='triadic'
-            onClick={handleStart}
-          >
-            {actionLabel}
-          </Button>
-        )}
+        {children}
       </S.RightSection>
     </S.Container>
   );
