2927800f7136b34c1d3886b832b612ce57a2b5c6
Seio924 <seio9241@gmail.com>
2025-03-10 12:39:32 +0900

    COC-183-feat(#119): 스페이스 입장 훅 구현

---
+++ src/hooks/space/useEnterSpace.ts
@@ -0,0 +1,25 @@
+import spaceApi from '@api/domain/space';
+import { ROUTES } from '@constants/path';
+import { SpaceStatusData } from '@customTypes/space';
+import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+
+interface EnterSpaceResponse {
+  status: SpaceStatusData;
+}
+
+export default function useEnterSpace(codingSpaceId: number) {
+  const navigate = useNavigate();
+
+  const mutation: UseMutationResult<EnterSpaceResponse, unknown, string> = useMutation({
+    mutationFn: spaceApi.enter,
+    onSuccess: ({ status }) => {
+      navigate(ROUTES.SPACE[status]({ codingSpaceId }), { replace: true });
+    },
+    onError: () => {
+      navigate(-1);
+    },
+  });
+
+  return { enterSpaceMutate: mutation.mutate, isLoading: mutation.isPending };
+}
