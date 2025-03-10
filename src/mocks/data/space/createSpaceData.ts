6db896fbbf829a5e1a106c9e2206d21b5b5bfced
Seio924 <seio9241@gmail.com>
2025-03-10 17:53:43 +0900

    COC-183-refactor(#119): mock data 분리

---
+++ src/mocks/data/space/createSpaceData.ts
@@ -0,0 +1,12 @@
+export const createSpaceResponse = {
+  code: 1200,
+  message: '코딩 스페이스 생성에 성공했습니다.',
+  result: {
+    codingSpaceId: 1,
+  },
+};
+
+export const createSpaceErrorResponse = {
+  code: 4200,
+  message: '유효성 검사 실패',
+};
