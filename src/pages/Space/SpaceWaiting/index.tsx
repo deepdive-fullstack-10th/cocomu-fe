c47d27d6e20ae7928436688c6dcbf5d4eade8644
Seio924 <seio9241@gmail.com>
2025-03-10 16:11:17 +0900

    COC-183-refactor(#119): SpaceWaiting 리팩토링

--- src/pages/Space/SpaceWaiting/index.tsx
+++ src/pages/Space/SpaceWaiting/index.tsx
@@ -1,3 +1,47 @@
+import { useParams } from 'react-router-dom';
+
+import useGetWaitingPage from '@hooks/space/useGetWaitingPage';
+
+import CodingWorkspace from '@components/Space/CodingWorkspace';
+import SpaceFooter from '@components/Space/SpaceFooter';
+import SpaceNavbar from '@components/Space/SpaceNavbar';
+
+import Loading from '@pages/Loading';
+
+import S from './style';
+
 export default function SpaceWaiting() {
-  return <div>SpaceWaiting</div>;
+  const { studyId, codingSpaceId } = useParams<{ studyId: string; codingSpaceId: string }>();
+  const { data, isLoading } = useGetWaitingPage(codingSpaceId);
+
+  const handleStart = () => {};
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <S.Container>
+      <SpaceNavbar
+        studyId={Number(studyId)}
+        name={data.name}
+        timer={data.codingMinutes}
+        isLeader={data.hostMe}
+        buttonLabel='문제 풀이 시작'
+        onClick={handleStart}
+      />
+
+      <CodingWorkspace
+        description={data.description}
+        workbookUrl={data.workbookUrl}
+        language={data.language}
+        activeUsers={data.activeUsers}
+        totalUserCount={data.totalUserCount}
+        disabled
+      />
+
+      <SpaceFooter
+        codingSpaceId={codingSpaceId}
+        testCases={data.testCases}
+      />
+    </S.Container>
+  );
 }
