f056062830f7597843036fac70ee3ec90c0e74f2
Seio924 <seio9241@gmail.com>
2025-03-10 16:09:45 +0900

    COC-183-feat(#119): CodingWorkspace 컴포넌트로 분리

---
+++ src/components/Space/CodingWorkspace/index.tsx
@@ -0,0 +1,56 @@
+import { useDraggable } from '@hooks/utils/useDraggable';
+
+import ProblemDescription from './ProblemDescription';
+import ResizablePanel from './ResizablePanel';
+import CodeExecutor, { CodeExecutorProps } from './CodeExecutor';
+
+import S from './style';
+
+interface CodingWorkspaceProps extends CodeExecutorProps {
+  description: string;
+  workbookUrl: string;
+}
+
+export default function CodingWorkspace({
+  description,
+  workbookUrl,
+  activeTabs,
+  language,
+  code,
+  onCodeChange,
+  disabled,
+  activeUsers,
+  totalUserCount,
+  setInput,
+  output,
+}: CodingWorkspaceProps) {
+  const { value: width, containerRef, handleMouseDown } = useDraggable({ direction: 'x' });
+
+  return (
+    <S.Container ref={containerRef}>
+      <ProblemDescription
+        description={description}
+        workbookUrl={workbookUrl}
+      />
+
+      <ResizablePanel
+        direction='y'
+        onMouseDown={handleMouseDown}
+      />
+
+      <S.CodeSection width={100 - width}>
+        <CodeExecutor
+          activeTabs={activeTabs}
+          language={language}
+          code={code}
+          onCodeChange={onCodeChange}
+          disabled={disabled}
+          activeUsers={activeUsers}
+          totalUserCount={totalUserCount}
+          setInput={setInput}
+          output={output}
+        />
+      </S.CodeSection>
+    </S.Container>
+  );
+}
