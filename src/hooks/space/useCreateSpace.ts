d04fcc8c169ddc8cc42c2bb7231084101435544a
Seio924 <seio9241@gmail.com>
2025-03-10 17:19:38 +0900

    COC-183-feat(#119): 스페이스 생성 시 캐시 무효화 추가

--- src/hooks/space/useCreateSpace.ts
+++ src/hooks/space/useCreateSpace.ts
@@ -1,14 +1,20 @@
 import spaceApi from '@api/domain/space';
 import { ROUTES } from '@constants/path';
-import { useMutation } from '@tanstack/react-query';
+import QUERY_KEYS from '@constants/queryKeys';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
 export default function useCreateSpace(studyId: number) {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const createSpaceMutate = useMutation({
     mutationFn: spaceApi.create,
     onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.SPACE_LIST, studyId],
+        exact: false,
+      });
       navigate(ROUTES.STUDY.DETAIL({ studyId }), { replace: true });
     },
   });
