e0cde9b963ac77cb2a31552e38901c98dca5a0a6
Seio924 <seio9241@gmail.com>
2025-03-10 17:53:43 +0900

    COC-183-refactor(#119): mock data 분리

---
+++ src/mocks/data/space/startSpaceData.ts
@@ -0,0 +1,12 @@
+export const startSpaceResponse = {
+  code: 4000,
+  message: '코딩 스페이스 시작에 성공했습니다.',
+  result: {
+    status: 'WAITING',
+  },
+};
+
+export const startSpaceErrorResponse = {
+  code: 4004,
+  message: '코딩 스페이스 시작에 실패했습니다.',
+};
