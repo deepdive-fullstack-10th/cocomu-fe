9b16592bea6194f07b3bbe871496e8c8ca22e5b8
Seio924 <seio9241@gmail.com>
2025-03-10 12:39:59 +0900

    COC-183-feat(#119): 스페이스 입장 mock data 설정

---
+++ src/mocks/data/space/enterSpaceData.ts
@@ -0,0 +1,12 @@
+export const enterSpaceResponse = {
+  code: 4000,
+  message: '코딩 스페이스 입장에 성공했습니다.',
+  result: {
+    status: 'WAITING',
+  },
+};
+
+export const enterSpaceErrorResponse = {
+  code: 4004,
+  message: '코딩 스페이스 입장에 실패했습니다.',
+};
