ea3d10ee90f8ff09dc6faff24833142f313ff51d
Seio924 <seio9241@gmail.com>
2025-03-10 16:11:17 +0900

    COC-183-refactor(#119): SpaceWaiting 리팩토링

--- src/pages/Space/SpaceWaiting/style.ts
+++ src/pages/Space/SpaceWaiting/style.ts
@@ -1,41 +1,15 @@
 import styled from '@emotion/styled';
 
-interface sizeStyleProps {
-  height?: number;
-}
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
 
-  height: 100%;
+  width: 100%;
+  height: 100vh;
 `;
 
-const ActiveUsersContainer = styled.div<sizeStyleProps>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  overflow-y: auto;
-
-  height: ${({ height }) => `${height}%` || '70%'};
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  scrollbar-width: none;
-  -ms-overflow-style: none;
-
-  user-select: none;
-`;
-
-const UserCount = styled.div`
-  ${({ theme }) => theme.font.heading[700]};
-  color: ${({ theme }) => theme.color.gray[800]};
-
-  margin-top: 5rem;
-`;
-
-const S = { Container, ActiveUsersContainer, UserCount };
+const S = {
+  Container,
+};
 
 export default S;
