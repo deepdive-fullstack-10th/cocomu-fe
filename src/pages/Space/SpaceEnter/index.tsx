9657efb6c8627abeb67eed90a7f1776f8cd8bd6a
Seio924 <seio9241@gmail.com>
2025-03-10 12:40:46 +0900

    COC-183-feat(#119): 스페이스 입장 페이지 구현

--- src/pages/Space/SpaceEnter/index.tsx
+++ src/pages/Space/SpaceEnter/index.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 
+import Loading from '@pages/Loading';
+import useEnterSpace from '@hooks/space/useEnterSpace';
+
 export default function SpaceEnter() {
+  const { codingSpaceId } = useParams();
+  const { enterSpaceMutate, isLoading } = useEnterSpace(Number(codingSpaceId));
+
+  useEffect(() => {
+    enterSpaceMutate(codingSpaceId);
+  }, []);
+
+  if (isLoading) return <Loading />;
+
   return <Outlet />;
 }
