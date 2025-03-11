db5932a31891933ef58daf51e8c82ac780633853
KimBeomJo <dongjun1315@naver.com>
2025-03-11 21:26:59 +0900

    COC-191-feat(#131): 메인 페이지 맨 위로 이동 버튼 추가

---
+++ src/components/_common/atoms/ScrollButton/style.ts
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+
+export const ScrollButton = styled.button`
+  position: fixed;
+  width: 8rem;
+  height: 8rem;
+  right: 5%;
+  bottom: 5%;
+
+  border-radius: 100rem;
+  border: 2px solid ${({ theme }) => theme.color.primary[600]};
+  background-color: ${({ theme }) => theme.color.gray[100]};
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.primary[50]};
+    transform: scale(1.1, 1.1);
+  }
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonText = styled.span`
+  ${({ theme }) => theme.font.heading[200]};
+  color: ${({ theme }) => theme.color.primary[600]};
+`;
+
+const S = {
+  ScrollButton,
+  ButtonGroup,
+  IconContainer,
+  ButtonText,
+};
+
+export default S;
