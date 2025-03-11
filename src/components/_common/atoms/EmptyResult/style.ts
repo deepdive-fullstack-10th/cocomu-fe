af0311b1b23cd3efe62c79670736cc4ef9c8dffb
Re-Note <dldydtn207@gmail.com>
2025-03-11 11:24:23 +0900

    COC-190-feat(#130): EmptyResult 컴포넌트 구현

---
+++ src/components/_common/atoms/EmptyResult/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const EmptyResultContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30rem;
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
