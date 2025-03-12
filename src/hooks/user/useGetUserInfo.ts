ad1782277b8a4ec7fbc02c3e11a99bff1f5b8aec
Seio924 <seio9241@gmail.com>
2025-03-12 14:27:05 +0900

    COC-198-feat(#137): 사용자 정보 조회 훅 구현

--- src/hooks/user/useGetUserInfo.ts
+++ src/hooks/user/useGetUserInfo.ts
@@ -3,10 +3,9 @@ import { useQuery } from '@tanstack/react-query';
 import QUERY_KEYS from '@constants/queryKeys';
 import userApi from '@api/domain/user';
 
-export default function useGetUserInfo({ enabled }: { enabled: boolean }) {
+export default function useGetUserInfo(userId: string) {
   return useQuery({
-    queryKey: [QUERY_KEYS.USER_DETAIL],
-    queryFn: async () => userApi.get(),
-    enabled,
+    queryKey: [QUERY_KEYS.USER_INFO, userId],
+    queryFn: () => userApi.getInfo(userId),
   });
 }
