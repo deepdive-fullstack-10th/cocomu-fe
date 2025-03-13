02b5e8367e40bb90ad3266124670cb99dd5f1b6b
HYJIN\isacc <isacc7224@gmail.com>
2025-03-13 17:04:02 +0900

    COC-188-feat(#128): 피드백 종료 구현

--- src/hooks/space/useFinishSpace.ts
+++ src/hooks/space/useFinishSpace.ts
@@ -1,23 +1,10 @@
-import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 
 import spaceApi from '@api/domain/space';
-import { WAITING_INFO } from '@constants/modal';
-import { useModalStore } from '@stores/useModalStore';
 
-export default function useFinishSpace(studyId: number, codingSpaceId: number) {
-  const navigate = useNavigate();
-  const { open } = useModalStore();
-
+export default function useFinishSpace() {
   const finishSpaceMutate = useMutation({
     mutationFn: spaceApi.finish,
-    onSuccess: () => {
-      open('waiting', {
-        label: WAITING_INFO.finish.label,
-        description: WAITING_INFO.finish.description,
-        navigate: navigate(WAITING_INFO.finish.navigate(studyId, codingSpaceId)),
-      });
-    },
   });
 
   return { finishSpaceMutate };
