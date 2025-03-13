1eb6c2bfa14053007178e901d1091971b8bda5e2
KimBeomJo <dongjun1315@naver.com>
2025-03-13 22:52:44 +0900

    COC-209-fix(#154): 참가 관련 로직 수정

--- src/components/Chat/ChatAlert/index.tsx
+++ src/components/Chat/ChatAlert/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import S from './style';
 
 interface ChatAlertProps {
@@ -10,29 +10,29 @@ export default function ChatAlert({ participant }: ChatAlertProps) {
   const isInitialRender = useRef(true);
   const prevParticipantsRef = useRef<string[]>([]);
 
+  const getDefaultAlert = useCallback(
+    (newParticipants: string[]): string =>
+      newParticipants.length === 1
+        ? `${newParticipants[0]} 님이 입장하셨습니다.`
+        : `${newParticipants.join(', ')} 님이 입장하셨습니다.`,
+    [],
+  );
+
   useEffect(() => {
     if (isInitialRender.current) {
       if (participant.length > 0) {
-        const initialAlert =
-          participant.length === 1
-            ? `${participant[0]} 님이 입장하였습니다.`
-            : `${participant.join(', ')} 님이 입장하였습니다.`;
-        setAlerts(initialAlert);
+        setAlerts(getDefaultAlert(participant));
       }
       isInitialRender.current = false;
     } else {
       const newParticipants = participant.filter((p) => !prevParticipantsRef.current.includes(p));
       if (newParticipants.length > 0) {
-        const newAlerts =
-          newParticipants.length === 1
-            ? `${newParticipants[0]} 님이 입장하였습니다.`
-            : `${newParticipants.join(', ')} 님이 입장하였습니다.`;
-        setAlerts(newAlerts);
+        setAlerts(getDefaultAlert(newParticipants));
       }
     }
 
     prevParticipantsRef.current = [...participant];
-  }, [participant]);
+  }, [participant, getDefaultAlert]);
 
   return (
     <S.TextWrapper>
