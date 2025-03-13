50ab0d2d74a6fb032438a748eaf683de634537cf
HYJIN\isacc <isacc7224@gmail.com>
2025-03-13 21:26:59 +0900

    COC-188-refactor(#128): 코드 저장 api 재요청 방지

--- src/hooks/space/useCodesave.ts
+++ src/hooks/space/useCodesave.ts
@@ -1,11 +1,26 @@
 import spaceApi from '@api/domain/space';
 import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function useSaveTabCode() {
+  const [isCooldown, setIsCooldown] = useState(false);
+
   const saveTabCodeMutate = useMutation({
-    mutationFn: ({ codingSpaceId, code }: { codingSpaceId: string; code: { code: string } }) =>
-      spaceApi.codesave(codingSpaceId, code),
+    mutationFn: async ({ codingSpaceId, code }: { codingSpaceId: string; code: { code: string } }) => {
+      if (isCooldown) {
+        return Promise.reject(new Error('Cooldown active'));
+      }
+
+      setIsCooldown(true);
+      const response = await spaceApi.codesave(codingSpaceId, code);
+
+      setTimeout(() => {
+        setIsCooldown(false);
+      }, 2000);
+
+      return response;
+    },
   });
 
-  return { saveTabCodeMutate };
+  return { saveTabCodeMutate, isCooldown };
 }
