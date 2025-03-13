37123684a7b527cfc4177d674c1e3b628bb6d19c
KimBeomJo <dongjun1315@naver.com>
2025-03-13 13:55:45 +0900

    COC-201-feat(#141): 채팅 입력 / 전송 로직 구현

--- src/components/_common/molecules/ChatInputField/index.tsx
+++ src/components/_common/molecules/ChatInputField/index.tsx
@@ -1,26 +1,43 @@
 import Icon from '@components/_common/atoms/Icon';
 import { BsSend } from 'react-icons/bs';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import S from './style';
 
 interface ChatInputFieldProps {
-  onSubmit?: () => void;
-  onClick?: () => void;
+  onSubmit?: (message: string) => void;
 }
 
-export default function ChatInputField({ onSubmit, onClick }: ChatInputFieldProps) {
+export default function ChatInputField({ onSubmit }: ChatInputFieldProps) {
+  const [chatContent, setChatContent] = useState<string>('');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setChatContent(e.target.value);
+  };
+
+  const handleSendChat = (e?: FormEvent<HTMLFormElement>) => {
+    if (e) e.preventDefault();
+    if (chatContent.trim()) {
+      onSubmit(chatContent);
+      setChatContent('');
+    }
+  };
+
   return (
-    <S.Container>
-      <S.ChatInputField
-        placeholder='Send Message'
-        onSubmit={onSubmit}
-      />
-      <Icon
-        size='md'
-        color='700'
-        onClick={onClick}
-      >
-        <BsSend />
-      </Icon>
-    </S.Container>
+    <form onSubmit={handleSendChat}>
+      <S.Container>
+        <S.ChatInputField
+          placeholder='Send Message'
+          onChange={handleInputChange}
+          value={chatContent}
+        />
+        <Icon
+          size='md'
+          color='700'
+          onClick={() => handleSendChat()}
+        >
+          <BsSend />
+        </Icon>
+      </S.Container>
+    </form>
   );
 }
