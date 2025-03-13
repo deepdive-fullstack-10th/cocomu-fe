9c9264bdf08a4c11366b54b700ec8093b9bacd1c
Seio924 <seio9241@gmail.com>
2025-03-13 14:03:51 +0900

    COC-202-refactor(#143): user api mock data 분리

---
+++ src/mocks/data/user/editUserData.ts
@@ -0,0 +1,10 @@
+export const editUserResponse = {
+  code: 3000,
+  message: '스터디 수정이 성공했습니다.',
+  result: 1,
+};
+
+export const editUserErrorResponse = {
+  code: 4200,
+  message: '스터디 수정이 실패했습니다.',
+};
