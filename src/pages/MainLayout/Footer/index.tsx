ee9bdebeb85f44d539f60b6aa435fbad60fcd7cc
KimBeomJo <dongjun1315@naver.com>
2025-03-13 12:07:03 +0900

    COC-203-fix(#144): 메뉴 아이템 변경 및 클릭이벤트 활성화

--- src/pages/MainLayout/Footer/index.tsx
+++ src/pages/MainLayout/Footer/index.tsx
@@ -1,9 +1,20 @@
-import MoveTopButton from '@components/_common/atoms/MoveTopButton';
 import FooterLabel from '@pages/MainLayout/Footer/FooterLabel';
 import { MENU_LIST } from '@constants/common';
 import S from './style';
 
 export default function Footer() {
+  const handleMenuClick = (index: number) => {
+    if (index === 0) {
+      /* console.log('이용약관 작성 중...'); */
+    } else if (index === 1) {
+      /* console.log('개인 정보 처리 방침 작성 중...'); */
+    } else if (index === 2) {
+      /* console.log('서비스 소개 작성 중...'); */
+    } else {
+      /* console.log('문의 게시판 개발 중...'); */
+    }
+  };
+
   return (
     <S.Container>
       <S.InfoSection>
@@ -25,7 +36,12 @@ export default function Footer() {
       </S.InfoSection>
       <S.MenuSection>
         {MENU_LIST.map((menuItem, index) => (
-          <S.MenuText key={`menu-${index + 1}`}>{menuItem}</S.MenuText>
+          <S.MenuText
+            key={`menu-${index + 1}`}
+            onClick={() => handleMenuClick(index)}
+          >
+            {menuItem}
+          </S.MenuText>
         ))}
       </S.MenuSection>
     </S.Container>
