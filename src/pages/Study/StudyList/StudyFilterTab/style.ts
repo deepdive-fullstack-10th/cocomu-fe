ed8c9394e56839af05041e83f1166c23874b40c7
Seio924 <seio9241@gmail.com>
2025-03-14 14:04:09 +0900

    COC-214-feat(#166): StudyList 반응형 구현

--- src/pages/Study/StudyList/StudyFilterTab/style.ts
+++ src/pages/Study/StudyList/StudyFilterTab/style.ts
@@ -6,11 +6,20 @@ const FilterTabContainer = styled.div`
   align-items: center;
 
   position: relative;
+
+  @media (max-width: 980px) {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 2rem;
+  }
 `;
 
 const DropdownWrapper = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: 2rem;
+
+  max-width: 100%;
 `;
 
 const S = {
